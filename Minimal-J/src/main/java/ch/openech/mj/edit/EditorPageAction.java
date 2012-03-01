9540d56421f191030e90c9057e713ff36e8f213d
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-03-01 11:18:20 +0100

    Back to original purpose of EditorPageAction

--- Minimal-J/src/main/java/ch/openech/mj/edit/EditorPageAction.java
+++ Minimal-J/src/main/java/ch/openech/mj/edit/EditorPageAction.java
@@ -1,71 +1,24 @@
 package ch.openech.mj.edit;
 
-import java.awt.event.ActionEvent;
+import ch.openech.mj.page.PageAction;
 
-import javax.swing.AbstractAction;
-
-import ch.openech.mj.edit.Editor.EditorFinishedListener;
-import ch.openech.mj.edit.form.FormVisual;
-import ch.openech.mj.page.PageContext;
-import ch.openech.mj.resources.ResourceHelper;
-import ch.openech.mj.resources.Resources;
-import ch.openech.mj.toolkit.ClientToolkit;
-import ch.openech.mj.toolkit.IComponent;
-import ch.openech.mj.toolkit.VisualDialog;
-
-public class EditorPageAction extends AbstractAction {
+public class EditorPageAction extends PageAction {
 	
-	private final Class<? extends Editor<?>> editorClass;
-	private final String[] parameter;
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		PageContext pageContext = ClientToolkit.getToolkit().findPageContext(e.getSource());
-		showPageOn(pageContext);
-	}
-
-	private void showPageOn(PageContext context) {
-		Editor<?> editor = createEditor(editorClass, parameter);
-		
-		FormVisual<?> form = editor.startEditor();
-		IComponent layout = ClientToolkit.getToolkit().createEditorLayout(editor.getInformation(), form, editor.getActions());
-		
-		final VisualDialog dialog = ClientToolkit.getToolkit().openDialog(context.getComponent(), layout, editor.getTitle());
-		dialog.setResizable(form.isResizable());
-		
-		editor.setEditorFinishedListener(new EditorFinishedListener() {
-			@Override
-			public void finished() {
-				dialog.closeDialog();
-			}
-		});
-		dialog.openDialog();
+	public EditorPageAction(Class<? extends Editor<?>> editorClass, String... args) {
+		super(EditorPage.class, getBaseName(editorClass), arguments(editorClass, args));
 	}
 	
-	static Editor<?> createEditor(Class<?> editorClass, String... arguments) {
-		try {
-			if (arguments.length > 0) {
-				Class<?>[] argumentClasses = new Class[arguments.length];
-				for (int i = 0; i<argumentClasses.length; i++) {
-					argumentClasses[i] = String.class;
-				}
-				return (Editor<?>) editorClass.getConstructor(argumentClasses).newInstance(arguments);
-			} else {
-				return (Editor<?>) editorClass.newInstance();
-			}
-		} catch (Exception x) {
-			throw new RuntimeException("EditorPage Erstellung fehlgeschlagen", x);
+	private static String[] arguments(Class<? extends Editor<?>> editor, String... args) {
+		// TODO Elegantere Kopiererei der Argument in EditorPageAction
+		String[] strings = new String[args.length + 1];
+		strings[0] = editor.getName();
+		for (int i = 0; i<args.length; i++) {
+			strings[i+1] = args[i];
 		}
-	}
-	
-	public EditorPageAction(Class<? extends Editor<?>> editorClass, String... parameter) {
-		this.editorClass = editorClass;
-		this.parameter = parameter;
-		ResourceHelper.initProperties(this, Resources.getResourceBundle(), getBaseName(editorClass));
+		return strings;
 	}
 
 	private static String getBaseName(Class<? extends Editor<?>> editorClass) {
 		return editorClass.getSimpleName();
 	}
-	
 }
