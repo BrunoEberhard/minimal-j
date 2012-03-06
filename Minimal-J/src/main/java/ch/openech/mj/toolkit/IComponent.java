5fa43b360ac1d848f3be008d770997fadfc9e1ba
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-03-06 13:02:36 +0100

    JavaDoced

--- Minimal-J/src/main/java/ch/openech/mj/toolkit/IComponent.java
+++ Minimal-J/src/main/java/ch/openech/mj/toolkit/IComponent.java
@@ -2,6 +2,18 @@ package ch.openech.mj.toolkit;
 
 /**
  * 
+ * This should be the uber-Interface of Vaadin and Swing components. Of corse thats not possible.
+ * So there is a sub Interface IComponentDelegate.<p>
+ * 
+ * The idea is:
+ * <ul>
+ * <li>If its a ComponentDelegate, than the getComponent() is called to get the component itself
+ * <li>If not, the class itself has to inherit from a component (mostly form some container)
+ * </ul>
+ * 
+ * <h3>warning</h3>
+ * Subject to change
+ * 
  * @author Bruno
  *
  */
