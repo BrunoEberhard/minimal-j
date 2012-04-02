52231bdee37db977e33e243fc9c5baad333a35c3
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-04-02 14:30:25 +0200

    SwingFlowField: Gap as border implemented

--- Minimal-J/src/main/java/ch/openech/mj/swing/toolkit/SwingFlowField.java
+++ Minimal-J/src/main/java/ch/openech/mj/swing/toolkit/SwingFlowField.java
@@ -8,7 +8,8 @@ import java.awt.event.MouseEvent;
 import java.util.List;
 
 import javax.swing.Action;
-import javax.swing.JComponent;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -19,9 +20,12 @@ import ch.openech.mj.util.StringUtils;
 
 
 public class SwingFlowField extends JPanel implements FlowField {
-
+	private final boolean vertical;
+	private JLabel lastLabel;
+	
 	public SwingFlowField(boolean vertical) {
 		super(new MigLayout(vertical ? "ins 4 4 0 0, gap 0 0, wrap 1" : "ins 0, gap 0 0"));
+		this.vertical = vertical;
 	}
 	
 	@Override
@@ -36,12 +40,15 @@ public class SwingFlowField extends JPanel implements FlowField {
 	
 	@Override
 	public void addAction(Action action) {
-		add(createLinkButton(action));
+		lastLabel = createLinkButton(action);
+		add(lastLabel);
 	}
 	
-	private static JComponent createLinkButton(final Action action) {
+	private static JLabel createLinkButton(final Action action) {
 		final JLabel label = new JLabel();
+		label.setIcon((Icon) action.getValue(Action.SMALL_ICON));
 		label.setText((String) action.getValue(Action.NAME));
+		label.setToolTipText((String) action.getValue(Action.SHORT_DESCRIPTION));
 		label.setForeground(Color.BLUE);
 		label.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		label.addMouseListener(new MouseAdapter() {
@@ -62,8 +69,8 @@ public class SwingFlowField extends JPanel implements FlowField {
 	@Override
 	public void addObject(Object object) {
 		if (object != null) {
-			JLabel label = new JLabel(object.toString());
-			add(label);
+			lastLabel = new JLabel(object.toString());
+			add(lastLabel);
 		}
 		revalidate();
 	}
@@ -71,16 +78,20 @@ public class SwingFlowField extends JPanel implements FlowField {
 	@Override
 	public void addHtml(String html) {
 		if (!StringUtils.isBlank(html)) {
-			JLabel label = new JLabel(html);
-			add(label);
+			lastLabel = new JLabel(html);
+			add(lastLabel);
 		}
 		revalidate();
 	}
 
 	@Override
 	public void addGap() {
-		JLabel label = new JLabel(" ");
-		add(label);
-		revalidate();
+		if (lastLabel != null) {
+			if (vertical) {
+				lastLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 8, 0));
+			} else {
+				lastLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 8));
+			}
+		}
 	}
 }
