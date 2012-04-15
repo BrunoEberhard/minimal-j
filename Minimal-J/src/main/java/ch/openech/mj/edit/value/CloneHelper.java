4f0f6a4cb1663323e5e6dbf3c8472869164e297b
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-04-15 13:31:23 +0200

    CloneHelper: tested and fixed

--- Minimal-J/src/main/java/ch/openech/mj/edit/value/CloneHelper.java
+++ Minimal-J/src/main/java/ch/openech/mj/edit/value/CloneHelper.java
@@ -2,10 +2,13 @@ package ch.openech.mj.edit.value;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import ch.openech.mj.db.model.AccessorInterface;
 import ch.openech.mj.db.model.ColumnAccess;
+import ch.openech.mj.db.model.ListColumnAccess;
 
 public class CloneHelper {
 
@@ -26,8 +29,14 @@ public class CloneHelper {
 	}
 
 	public static void deepCopy(Object from, Object to) {
-		Map<String, AccessorInterface> accessors = ColumnAccess.getAccessors(from.getClass());
-		for (AccessorInterface accessor : accessors.values()) {
+		deepCopyListFields(from, to);
+		deepCopyNonListFields(from, to);
+	}
+
+	private static void deepCopyNonListFields(Object from, Object to) {
+		List<String> keys = ColumnAccess.getNonListKeys(from.getClass());
+		for (String key: keys) {
+			AccessorInterface accessor = ColumnAccess.getAccessors(from.getClass()).get(key);
 			Object fromValue = accessor.getValue(from);
 			if (accessor.isFinal()) {
 				Object toValue = accessor.getValue(to);
@@ -37,6 +46,23 @@ public class CloneHelper {
 				accessor.setValue(to, copyValue);
 			} else {
 				accessor.setValue(to, fromValue);
+			}
+		}
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static void deepCopyListFields(Object from, Object to) {
+		Map<String, AccessorInterface> listAccessors = ListColumnAccess.getAccessors(from.getClass());
+		for (AccessorInterface accessor : listAccessors.values()) {
+			List fromValue = (List)accessor.getValue(from);
+			if (fromValue == null) continue;
+			List toValue = (List)accessor.getValue(to);
+			if (!accessor.isFinal()) {
+				toValue = new ArrayList();
+				accessor.setValue(to, toValue);
+			}
+			for (Object element : fromValue) {
+				toValue.add(clone(element));
 			}
 		}
 	}
