0c325f16282fb61f378a0a8b05d003d880809977
Bruno Eberhard <bruno.eberhard@pop.ch>
2012-07-05 18:40:40 +0200

    FormField: class comment

--- Minimal-J/src/main/java/ch/openech/mj/edit/fields/FormField.java
+++ Minimal-J/src/main/java/ch/openech/mj/edit/fields/FormField.java
@@ -3,6 +3,12 @@ package ch.openech.mj.edit.fields;
 import ch.openech.mj.edit.Value;
 import ch.openech.mj.toolkit.IComponent;
 
+/**
+ * A read only / display Field in a Form.<br>
+ * note: A FormField is not a component but provides a component
+ *
+ * @param <T> The type of the value of this field.
+ */
 public interface FormField<T> extends Value<T> {
 
 	public IComponent getComponent();
