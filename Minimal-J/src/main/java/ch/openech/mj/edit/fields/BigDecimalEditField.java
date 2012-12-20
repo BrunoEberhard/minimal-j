a585121ecdcd9a094d57d17e25c4abfada66aab9
bruno <bruno.eberhard@pop.ch>
2012-12-20 17:41:44 +0100

    BigDecimalEditField: support for decimalPlaces

--- Minimal-J/src/main/java/ch/openech/mj/edit/fields/BigDecimalEditField.java
+++ Minimal-J/src/main/java/ch/openech/mj/edit/fields/BigDecimalEditField.java
@@ -1,15 +1,17 @@
 package ch.openech.mj.edit.fields;
 
 import java.math.BigDecimal;
+import java.util.Random;
 
+import ch.openech.mj.autofill.DemoEnabled;
 import ch.openech.mj.db.model.InvalidValues;
 import ch.openech.mj.db.model.PropertyInterface;
 
 
-public class BigDecimalEditField extends NumberEditField<BigDecimal> /* implements DemoEnabled */ {
+public class BigDecimalEditField extends NumberEditField<BigDecimal> implements DemoEnabled {
 
-	public BigDecimalEditField(PropertyInterface property, int size, boolean negative) {
-		super(property, size, 0, negative);
+	public BigDecimalEditField(PropertyInterface property, int size, int decimalPlaces, boolean negative) {
+		super(property, size, decimalPlaces, negative);
 	}
 
 	@Override
@@ -39,15 +41,16 @@ public class BigDecimalEditField extends NumberEditField<BigDecimal> /* implemen
 		}
 	}
 
-//	@Override
-//	public void fillWithDemoData() {
-//		Random random = new Random();
-//		int value = random.nextInt(10 ^ size);
-//		if (!negative || random.nextBoolean()) {
-//			setObject(value);
-//		} else {
-//			setObject(-value);
-//		}
-//	}
+	@Override
+	public void fillWithDemoData() {
+		// TODO check for valid ranges for size-decimalPlaces
+		Random random = new Random();
+		double value = random.nextDouble() * (10 ^ (size - decimalPlaces));
+		if (!negative || random.nextBoolean()) {
+			setObject(BigDecimal.valueOf(value));
+		} else {
+			setObject(BigDecimal.valueOf(-value));
+		}
+	}
 
 }
