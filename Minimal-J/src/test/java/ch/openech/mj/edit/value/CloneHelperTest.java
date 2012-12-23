c75c3d484e61519e394f17b458966f3ecffc96f9
bruno <bruno.eberhard@pop.ch>
2012-12-23 07:04:33 +0100

    CloneHelperTest: Added assertion for transient values

--- Minimal-J/src/test/java/ch/openech/mj/edit/value/CloneHelperTest.java
+++ Minimal-J/src/test/java/ch/openech/mj/edit/value/CloneHelperTest.java
@@ -15,6 +15,7 @@ public class CloneHelperTest {
 	public void testClone() {
 		CloneHelperTestA a = new CloneHelperTestA();
 		a.a = "Hallo";
+		a.t = "Transient";
 
 		CloneHelperTestB b = new CloneHelperTestB();
 		b.b = "Du";
@@ -22,18 +23,22 @@ public class CloneHelperTest {
 
 		CloneHelperTestC c = new CloneHelperTestC();
 		c.c = "Test";
+		c.d = new CloneHelperTestD();
+		c.d.d = 23;
 		a.c.add(c);
 		
 		CloneHelperTestA a2 = CloneHelper.clone(a);
 
 		// Prüfen ob richtig kopiert wurde
 		Assert.assertEquals(a.a, a2.a);
+		Assert.assertEquals("Transient values not cloned!", a.t, a2.t);
 		
 		Assert.assertEquals(a.b.size(), a2.b.size());
 		Assert.assertEquals(a.b.get(0).b, a2.b.get(0).b);
 
 		Assert.assertEquals(a.c.size(), a2.c.size());
 		Assert.assertEquals(a.c.get(0).c, a2.c.get(0).c);
+		Assert.assertEquals(a.c.get(0).d.d, a2.c.get(0).d.d);
 
 		Assert.assertEquals(null, a2.empty);
 	}
@@ -43,6 +48,7 @@ public class CloneHelperTest {
 		public final List<CloneHelperTestB> b = new ArrayList<CloneHelperTestB>();
 		public List<CloneHelperTestC> c = new ArrayList<CloneHelperTestC>();
 		public List<CloneHelperTestC> empty;
+		public transient String t;
 	}
 
 	public static class  CloneHelperTestB {
@@ -51,6 +57,11 @@ public class CloneHelperTest {
 
 	public static class  CloneHelperTestC {
 		public String c;
+		public CloneHelperTestD d;
+	}
+	
+	public static class  CloneHelperTestD {
+		public Integer d;
 	}
 
 }
