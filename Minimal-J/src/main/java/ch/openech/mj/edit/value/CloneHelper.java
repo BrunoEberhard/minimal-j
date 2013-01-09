b18949b3b7a5a9740b3b0b0754e37228a9583e01
bruno <bruno.eberhard@pop.ch>
2013-01-09 17:40:03 +0100

    CloneHelper: deepClone clones subobjects not only copies its content as deepCopy does

--- Minimal-J/src/main/java/ch/openech/mj/edit/value/CloneHelper.java
+++ Minimal-J/src/main/java/ch/openech/mj/edit/value/CloneHelper.java
@@ -17,7 +17,7 @@ public class CloneHelper {
 		Class<T> clazz = (Class<T>) object.getClass();
 		try {
 			T copy = newInstance(clazz);
-			deepCopy(object, copy);
+			deepClone(object, copy);
 			return copy;
 		} catch (SecurityException e) {
 			throw new RuntimeException(e);
@@ -26,6 +26,45 @@ public class CloneHelper {
 		}
 	}
 
+	public static void deepClone(Object from, Object to) {
+		try {
+			_deepClone(from, to);
+		} catch (IllegalAccessException | IllegalArgumentException x) {
+			throw new RuntimeException(x);
+		}
+	}
+	
+	private static void _deepClone(Object from, Object to) throws IllegalArgumentException, IllegalAccessException {
+		for (Field field : from.getClass().getDeclaredFields()) {
+			if (FieldUtils.isStatic(field)) continue;
+			field.setAccessible(true);
+			Object fromValue = field.get(from);
+			Object toValue = field.get(to);
+			if (FieldUtils.isList(field.getType())) {
+				List fromList = (List)field.get(from);
+				if (fromList == null) continue;
+				List toList = (List)toValue;
+				if (!FieldUtils.isFinal(field)) {
+					toList = new ArrayList();
+					field.set(to, toList);
+				} else {
+					toList.clear();
+				}
+				for (Object element : fromList) {
+					toList.add(clone(element));
+				}
+			} else if (isPrimitive(field) || FieldUtils.isTransient(field) || fromValue == null) {
+				// note: transient fields are not cloned
+				field.set(to, fromValue);
+			} else if (FieldUtils.isFinal(field) && toValue != null) {
+				deepClone(fromValue, toValue);
+			} else {
+				Object copyValue = CloneHelper.clone(fromValue);
+				field.set(to, copyValue);
+			}
+		}
+	}
+	
 	public static void deepCopy(Object from, Object to) {
 		try {
 			_deepCopy(from, to);
@@ -56,11 +95,11 @@ public class CloneHelper {
 			} else if (isPrimitive(field) || FieldUtils.isTransient(field) || fromValue == null) {
 				// note: transient fields are not cloned
 				field.set(to, fromValue);
-			} else if (FieldUtils.isFinal(field) && toValue != null) {
-				deepCopy(fromValue, toValue);
+			} else if (fromValue != null && toValue == null) {
+				toValue = CloneHelper.clone(fromValue);
+				field.set(to, toValue);
 			} else {
-				Object copyValue = CloneHelper.clone(fromValue);
-				field.set(to, copyValue);
+				deepCopy(fromValue, toValue);
 			}
 		}
 	}
