0e38906afc6e3b28649ca2ea41a020d38a29a3bc
bruno <bruno.eberhard@pop.ch>
2013-01-24 07:21:08 +0100

    ApplicationContext: removed concept of PreferenceListener

--- Minimal-J/src/main/java/ch/openech/mj/application/ApplicationContext.java
+++ Minimal-J/src/main/java/ch/openech/mj/application/ApplicationContext.java
@@ -1,13 +1,8 @@
 package ch.openech.mj.application;
 
-import java.lang.ref.SoftReference;
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class ApplicationContext {
 
 	private Object preferences;
-	private List<SoftReference<PreferenceChangeListener>> listeners = new ArrayList<SoftReference<PreferenceChangeListener>>();
 
 	public abstract void setUser(String user);
 
@@ -37,27 +32,4 @@ public abstract class ApplicationContext {
 	public abstract void loadPreferences(Object preferences);
 
 	public abstract void savePreferences(Object preferences);
-
-	public void addPreferenceChangeListener(PreferenceChangeListener preferenceChangeListener) {
-		SoftReference<PreferenceChangeListener> softReference = new SoftReference<PreferenceChangeListener>(
-				preferenceChangeListener);
-		listeners.add(softReference);
-	}
-
-	public void preferenceChange() {
-		for (int i = listeners.size() - 1; i >= 0; i--) {
-			SoftReference<PreferenceChangeListener> softReference = listeners.get(i);
-			PreferenceChangeListener preferenceChangeListener = softReference.get();
-			if (preferenceChangeListener != null) {
-				preferenceChangeListener.preferenceChange();
-			} else {
-				listeners.remove(i);
-			}
-		}
-	}
-
-	private static interface PreferenceChangeListener {
-		void preferenceChange();
-	}
-
 }
