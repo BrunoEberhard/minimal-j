5600790a80cd7d9c5895c576553438d3eb307416
bruno <bruno.eberhard@pop.ch>
2013-02-02 14:50:11 +0100

    Code: Commented

--- Minimal-J/src/main/java/ch/openech/mj/model/annotation/Code.java
+++ Minimal-J/src/main/java/ch/openech/mj/model/annotation/Code.java
@@ -3,6 +3,11 @@ package ch.openech.mj.model.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Marks a property to be a code. Codes must be registred at the
+ * <code>Codes</code> class.
+ * 
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Code {
 
