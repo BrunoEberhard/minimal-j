202c0f0e9e7f95b73aa149f4ebb4c43b3e80e319
bruno <bruno.eberhard@pop.ch>
2013-04-28 10:55:29 +0200

    Validatable: JavaDoc correction

--- Minimal-J/src/main/java/ch/openech/mj/edit/validation/Validatable.java
+++ Minimal-J/src/main/java/ch/openech/mj/edit/validation/Validatable.java
@@ -1,13 +1,9 @@
 package ch.openech.mj.edit.validation;
 
-
-
-
 public interface Validatable {
 
 	/**
 	 * 
-	 * @param key Key of the field to validate or <code>null</code> to validate entier object
 	 * @return ValidationMessage or <code>null</code> if valid
 	 */
 	String validate();
