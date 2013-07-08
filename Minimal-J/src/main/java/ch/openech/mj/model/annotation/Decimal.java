52f7d47a71b900be60594a39b1ae6146b2f01455
bruno <bruno.eberhard@pop.ch>
2013-07-08 07:31:18 +0200

    Decimal: comment

--- Minimal-J/src/main/java/ch/openech/mj/model/annotation/Decimal.java
+++ Minimal-J/src/main/java/ch/openech/mj/model/annotation/Decimal.java
@@ -3,6 +3,9 @@ package ch.openech.mj.model.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Number of decimal places for BigDecimal
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Decimal {
 
