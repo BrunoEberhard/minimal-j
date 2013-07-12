e0b99a4fb15472c503af2be0c05ce5adcb04247f
bruno <bruno.eberhard@pop.ch>
2013-07-12 07:30:52 +0200

    Size: Removed SizeImpl. Concept not used anymore

--- Minimal-J/src/main/java/ch/openech/mj/model/annotation/Size.java
+++ Minimal-J/src/main/java/ch/openech/mj/model/annotation/Size.java
@@ -1,6 +1,5 @@
 package ch.openech.mj.model.annotation;
 
-import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
@@ -12,25 +11,5 @@ public @interface Size {
 	public static final int TIME_HH_MM = 4;
 	public static final int TIME_WITH_SECONDS = 6;
 	public static final int TIME_WITH_MILLIS = 9;
-	
-	public static class SizeImpl implements Size {
-
-		private final int size;
 		
-		public SizeImpl(int size) {
-			this.size = size;
-		}
-
-		@Override
-		public Class<? extends Annotation> annotationType() {
-			return Size.class;
-		}
-
-		@Override
-		public int value() {
-			return size;
-		}
-	}
-	
-	
 }
