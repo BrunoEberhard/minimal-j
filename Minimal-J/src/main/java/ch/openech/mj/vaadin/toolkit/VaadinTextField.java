9f843f35a276c6caec139faab2092409e6f1483a
bruno <bruno.eberhard@pop.ch>
2013-09-03 14:14:24 +0200

    Regression Fix for Layout with VaadinTextField

--- Minimal-J/src/main/java/ch/openech/mj/vaadin/toolkit/VaadinTextField.java
+++ Minimal-J/src/main/java/ch/openech/mj/vaadin/toolkit/VaadinTextField.java
@@ -7,51 +7,34 @@ import ch.openech.mj.toolkit.TextField;
 
 import com.vaadin.event.FieldEvents.TextChangeEvent;
 import com.vaadin.event.FieldEvents.TextChangeListener;
-import com.vaadin.ui.HorizontalLayout;
 
 /**
- * A Vaadin Widget must not implement strange interfaces (meaning our TextField) because
- * it would not be recognized by the gwt compiler. So this VaadinTextField cannot extend
- * the vaadin.TextField and implement TextField directly. Everything is delegated.
  * 
  * @author Bruno
  *
  */
-public class VaadinTextField extends HorizontalLayout implements TextField {
+public class VaadinTextField extends com.vaadin.ui.TextField implements TextField {
 
-	private final com.vaadin.ui.TextField textWidget;
 	private TextChangeEvent event;
 	
-	public VaadinTextField() {
-		this(null, new com.vaadin.ui.TextField());
-	}
-	
 	public VaadinTextField(InputComponentListener changeListener, int maxLength) {
-		this(changeListener, new com.vaadin.ui.TextField());
-		textWidget.setMaxLength(maxLength);
+		this(changeListener, maxLength, null);
 	}
 	
 	public VaadinTextField(InputComponentListener changeListener, int maxLength, String allowedCharacters) {
-		this(changeListener, new com.vaadin.ui.TextField());
-		textWidget.setMaxLength(maxLength);
-	}
-	
-	private VaadinTextField(InputComponentListener changeListener, com.vaadin.ui.TextField vaadinTextWidget) {
-		textWidget = vaadinTextWidget;
-		textWidget.setNullRepresentation("");
-		textWidget.setImmediate(true);
+		setMaxLength(maxLength);
+		setNullRepresentation("");
+		setImmediate(true);
 		if (changeListener != null) {
-			textWidget.addListener(new VaadinTextFieldTextChangeListener(changeListener));
+			addListener(new VaadinTextFieldTextChangeListener(changeListener));
 		} else {
-			textWidget.setReadOnly(true);
+			setReadOnly(true);
 		}
-		addComponent(textWidget);
-		setWidth(100, UNITS_PERCENTAGE);
 	}
 
 	@Override
 	public void setText(String text) {
-		textWidget.setValue(text);
+		setValue(text);
 	}
 
 	@Override
@@ -59,7 +42,7 @@ public class VaadinTextField extends HorizontalLayout implements TextField {
 		if (event != null) {
 			return event.getText();
 		} else {
-			return (String) textWidget.getValue();
+			return (String) getValue();
 		}
 	}
 
