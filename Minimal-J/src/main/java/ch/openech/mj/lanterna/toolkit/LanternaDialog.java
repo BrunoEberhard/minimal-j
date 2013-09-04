79c0530e87dc1b3012edacfd1e749a184749be68
bruno <bruno.eberhard@pop.ch>
2013-09-04 19:53:28 +0200

    Lanterna adapted to the new ClientToolkit

---
+++ Minimal-J/src/main/java/ch/openech/mj/lanterna/toolkit/LanternaDialog.java
@@ -0,0 +1,52 @@
+package ch.openech.mj.lanterna.toolkit;
+
+import ch.openech.mj.toolkit.IAction;
+import ch.openech.mj.toolkit.IComponent;
+import ch.openech.mj.toolkit.IDialog;
+
+import com.googlecode.lanterna.gui.Component;
+import com.googlecode.lanterna.gui.GUIScreen;
+import com.googlecode.lanterna.gui.Window;
+import com.googlecode.lanterna.gui.listener.WindowAdapter;
+
+public class LanternaDialog implements IDialog {
+
+	private final GUIScreen screen;
+	private final Component content;
+	private final String title;
+	private Window window;
+	
+	private CloseListener closeListener;
+	
+	public LanternaDialog(GUIScreen screen, IComponent content, String title, IAction[] actions) {
+		this.screen = screen;
+		this.content = new LanternaEditorLayout(content, actions);
+		this.title = title;
+	}
+	
+	@Override
+	public void setCloseListener(CloseListener closeListener) {
+		this.closeListener = closeListener;
+	}
+
+	@Override
+	public void openDialog() {
+		window = new Window(title);
+		window.addComponent(content);
+		window.addWindowListener(new WindowAdapter() {
+			@Override
+			public void onWindowClosed(Window window) {
+				if (closeListener != null) {
+					closeListener.close();
+				}
+			}
+		});
+		screen.showWindow(window);
+	}
+
+	@Override
+	public void closeDialog() {
+		window.close();
+	}
+
+}
---
+++ Minimal-J/src/main/java/ch/openech/mj/lanterna/toolkit/LanternaTextField.java
@@ -0,0 +1,84 @@
+package ch.openech.mj.lanterna.toolkit;
+
+import java.awt.event.FocusListener;
+
+import ch.openech.mj.toolkit.ClientToolkit.InputComponentListener;
+import ch.openech.mj.toolkit.TextField;
+
+import com.googlecode.lanterna.gui.component.InteractableComponent;
+import com.googlecode.lanterna.gui.component.TextBox;
+import com.googlecode.lanterna.gui.listener.ComponentAdapter;
+import com.googlecode.lanterna.input.Key;
+
+public class LanternaTextField extends TextBox implements TextField {
+
+	private final InputComponentListener changeListener;
+	private FocusListener focusListener;
+	
+	public LanternaTextField(InputComponentListener changeListener) {
+		this.changeListener = changeListener;
+		addComponentListener(new TextFieldComponentListener());
+	}
+
+	@Override
+	public void setEditable(boolean editable) {
+		super.setVisible(editable);
+	}
+
+	@Override
+	public void setFocusListener(FocusListener focusListener) {
+		this.focusListener = focusListener;
+	}
+
+	@Override
+	public void setCommitListener(Runnable listener) {
+		// ignored at the moment
+	}
+
+	@Override
+	public String getText() {
+		String text = super.getText();
+		if (text.length() == 0) return null;
+		return text;
+	}
+
+	@Override
+	public void setText(String text) {
+		if (text == null) {
+			text = "";
+		}
+		super.setText(text);
+	}
+	
+	private void fireChangeEvent() {
+		changeListener.changed(LanternaTextField.this);
+	}
+	
+	@Override
+	public Result keyboardInteraction(Key key) {
+		Result result = super.keyboardInteraction(key);
+		if (result != Result.EVENT_NOT_HANDLED) {
+			fireChangeEvent();
+		}
+		return result;
+	}
+
+	private class TextFieldComponentListener extends ComponentAdapter {
+
+		@Override
+		public void onComponentReceivedFocus(InteractableComponent interactableComponent) {
+			if (focusListener != null) {
+				focusListener.focusGained(null);
+			}
+		}
+		
+		@Override
+		public void onComponentLostFocus(InteractableComponent interactableComponent) {
+			if (focusListener != null) {
+				focusListener.focusLost(null);
+			}
+		}
+		
+	}
+	
+}
