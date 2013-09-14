f48425ea80f0f18ce1bea805990c1fe65d6b9284
bruno <bruno.eberhard@pop.ch>
2013-09-14 17:22:27 +0200

    Replaced MigLayout with proprietary Layout (minus one jar)

--- Minimal-J/src/main/java/ch/openech/mj/swing/toolkit/SwingFlowField.java
+++ Minimal-J/src/main/java/ch/openech/mj/swing/toolkit/SwingFlowField.java
@@ -1,12 +1,17 @@
 package ch.openech.mj.swing.toolkit;
 
 import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import java.awt.Rectangle;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import net.miginfocom.swing.MigLayout;
 import ch.openech.mj.toolkit.FlowField;
 import ch.openech.mj.toolkit.IComponent;
 
@@ -15,7 +20,7 @@ public class SwingFlowField extends JPanel implements FlowField {
 	private JLabel lastLabel;
 	
 	public SwingFlowField() {
-		super(new MigLayout("ins 4 1 0 0, gap 0 0, wrap 1"));
+		super(new FlowLayoutManager());
 	}
 	
 	@Override
@@ -39,9 +44,57 @@ public class SwingFlowField extends JPanel implements FlowField {
 
 	@Override
 	public void add(IComponent component) {
-		super.add((Component) component);
+		super.add((Component) component, ""); // empty string need otherwise LayoutManager doesn't get the component
 		repaint();
 		revalidate();
 	}
 	
+	private static class FlowLayoutManager implements LayoutManager {
+
+		private final List<Component> components = new LinkedList<>();
+		private Dimension size;
+		private Rectangle lastParentBounds = null;
+		
+		public FlowLayoutManager() {
+		}
+
+		@Override
+		public Dimension preferredLayoutSize(Container parent) {
+			layoutContainer(parent);
+			return size;
+		}
+
+		@Override
+		public Dimension minimumLayoutSize(Container parent) {
+			layoutContainer(parent);
+			return size;
+		}
+
+		@Override
+		public void layoutContainer(Container parent) {
+			if (lastParentBounds != null && lastParentBounds.equals(parent.getBounds())) return;
+			lastParentBounds = parent.getBounds();
+			
+			int y = 4;
+			int x = 1;
+			int width = parent.getWidth();
+			int widthWithoutIns = width - x;
+			for (Component component : components) {
+				int height = component.getPreferredSize().height;
+				component.setBounds(x, y, widthWithoutIns, height);
+				y += height;
+			}
+			size = new Dimension(width, y);
+		}
+
+		@Override
+		public void addLayoutComponent(String name, Component comp) {
+			components.add(comp);
+		}
+
+		@Override
+		public void removeLayoutComponent(Component comp) {
+			components.remove(comp);
+		}
+	}
 }
--- Minimal-J/src/main/java/ch/openech/mj/swing/toolkit/SwingGridFormLayout.java
+++ Minimal-J/src/main/java/ch/openech/mj/swing/toolkit/SwingGridFormLayout.java
@@ -1,11 +1,19 @@
 package ch.openech.mj.swing.toolkit;
 
 import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FontMetrics;
+import java.awt.LayoutManager2;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JPanel;
 
-import net.miginfocom.swing.MigLayout;
 import ch.openech.mj.toolkit.GridFormLayout;
 import ch.openech.mj.toolkit.IComponent;
 
@@ -13,13 +21,8 @@ public class SwingGridFormLayout extends JPanel implements GridFormLayout {
 	private final int columnWidth;
 	
 	public SwingGridFormLayout(int columns, int columnWidthPercentage) {
-		String columnConstraints = "";
 		columnWidth = getColumnWidth() * columnWidthPercentage / 100;
-		for (int i = 0; i<columns; i++) {
-			columnConstraints += "[grow 1, push, sizegroup c]";
-		}
-		setLayout(new MigLayout("ins 5, gapy 0px, wrap " + columns, columnConstraints));
-		
+		setLayout(new GridFormLayoutManager(columns, columnWidth, 5));
 		setBorder(null);
 	}
 	
@@ -31,12 +34,172 @@ public class SwingGridFormLayout extends JPanel implements GridFormLayout {
 	@Override
 	public void add(IComponent c, int span) {
 		Component component = (Component) c;
-		int w = span * columnWidth; // minimum und prefered size
-		if (SwingClientToolkit.verticallyGrowing(component)) {
-			add(component, "spanx " + span + ", growx, aligny top, width " + w + "px:" + w + "px");
-		} else {
-			add(component, "spanx " + span + ", growx, aligny top, sizegroupy h, width " + w + "px:" + w + "px");
-		}
+		add(component, new GridFormLayoutConstraint(span, SwingClientToolkit.verticallyGrowing(component)));
 	}
 
+	private static class GridFormLayoutConstraint {
+	
+		private final int span;
+		private final boolean verticallyGrowing;
+		
+		public GridFormLayoutConstraint(int span, boolean verticallyGrowing) {
+			super();
+			this.span = span;
+			this.verticallyGrowing = verticallyGrowing;
+		}
+
+		protected int getSpan() {
+			return span;
+		}
+
+		protected boolean isVerticallyGrowing() {
+			return verticallyGrowing;
+		}
+		
+	}
+	
+	private static class GridFormLayoutManager implements LayoutManager2 {
+
+		private final int columns;
+		private final int minColumnWidth;
+		private final int ins;
+		private final List<List<Component>> rows = new LinkedList<>();
+		private final Map<Component, GridFormLayoutConstraint> constraints = new HashMap<>();
+		
+		private Dimension size;
+		private Rectangle lastParentBounds = null;
+		private int column = Integer.MAX_VALUE;
+		
+		public GridFormLayoutManager(int columns, int minColumnWidth, int ins) {
+			this.columns = columns;
+			this.minColumnWidth = minColumnWidth;
+			this.ins = ins;
+		}
+
+		@Override
+		public Dimension preferredLayoutSize(Container parent) {
+			layoutContainer(parent);
+			return size;
+		}
+
+		@Override
+		public Dimension minimumLayoutSize(Container parent) {
+			layoutContainer(parent);
+			return size;
+		}
+		
+		@Override
+		public void layoutContainer(Container parent) {
+			if (lastParentBounds != null && lastParentBounds.equals(parent.getBounds())) {
+				return;
+			}
+			lastParentBounds = parent.getBounds();
+			
+			int fixHeight = calcFixHeight();
+			int y = ins;
+			int width = parent.getWidth();
+			int widthWithoutIns = width - 2 * ins;
+			for (List<Component> row : rows) {
+				layoutRow(widthWithoutIns, row, y);
+				if (isRowVerticallyGrowing(row)) {
+					y += getHeight(row);
+				} else {
+					y += fixHeight;
+				}
+			}
+			y+= ins;
+			size = new Dimension(Math.max(minColumnWidth * columns, width), y);
+		}
+
+		private void layoutRow(int width, List<Component> row, int y) {
+			int x = ins;
+			for (Component component : row) {
+				component.setLocation(x, y);
+				GridFormLayoutConstraint constraint = constraints.get(component);
+				int componentWidth = constraint.getSpan() * width / columns;
+				x += componentWidth; 
+				component.setSize(componentWidth, component.getPreferredSize().height);
+			}
+		}
+		
+		private int getHeight(List<Component> row) {
+			int height = 0;
+			for (Component component : row) {
+				height = Math.max(height, component.getPreferredSize().height);
+			}
+			return height;
+		}
+		
+		private boolean isRowVerticallyGrowing(List<Component> row) {
+			for (Component component : row) {
+				GridFormLayoutConstraint constraint = constraints.get(component);
+				if (constraint.isVerticallyGrowing()) {
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		private int calcFixHeight() {
+			int height = 0;
+			for (List<Component> row : rows) {
+				for (Component component : row) {
+					if (!SwingClientToolkit.verticallyGrowing(component)) {
+						height = Math.max(height, component.getPreferredSize().height);
+					}
+				}
+			}
+			return height;
+		}
+
+		@Override
+		public void addLayoutComponent(Component comp, Object constraint) {
+			GridFormLayoutConstraint formConstraint = (GridFormLayoutConstraint) constraint;
+			constraints.put(comp, formConstraint);
+			List<Component> row;
+			if (column >= columns) {
+				row = new ArrayList<>();
+				rows.add(row);
+				column = 0;
+			} else {
+				row = rows.get(rows.size()-1);
+			}
+			row.add(comp);
+			column += formConstraint.getSpan();
+		}
+
+		@Override
+		public Dimension maximumLayoutSize(Container target) {
+			layoutContainer(target);
+			return size;
+
+		}
+
+		@Override
+		public void invalidateLayout(Container target) {
+			// TODO Auto-generated method stub
+		}
+
+		
+		@Override
+		public float getLayoutAlignmentX(Container target) {
+			return 0;
+		}
+
+		@Override
+		public float getLayoutAlignmentY(Container target) {
+			return 0;
+		}
+
+		@Override
+		public void addLayoutComponent(String name, Component comp) {
+			// not used
+		}
+
+		@Override
+		public void removeLayoutComponent(Component comp) {
+			// not used
+		}
+	}
+	
 }
