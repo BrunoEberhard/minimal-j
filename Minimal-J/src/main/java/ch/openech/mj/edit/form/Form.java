3421802ae468409b6efd7f5d9a4a4c58618c25d0
bruno <bruno.eberhard@pop.ch>
2013-11-05 18:51:43 +0100

    Form.line: check if number of columns is ok

--- Minimal-J/src/main/java/ch/openech/mj/edit/form/Form.java
+++ Minimal-J/src/main/java/ch/openech/mj/edit/form/Form.java
@@ -244,6 +244,7 @@ public class Form<T> implements IForm<T>, DemoEnabled {
 //	}
 	
 	public void line(Object... keys) {
+		if (keys.length > columns) throw new IllegalArgumentException("More keys than specified in the constructor");
 		int span = columns / keys.length;
 		int rest = columns;
 		for (int i = 0; i<keys.length; i++) {
