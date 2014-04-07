d28fd4a3a4bcfb846e48edaae354ddb01430471b
bruno <bruno.eberhard@pop.ch>
2014-04-07 20:10:14 +0200

    Introduced concept of 'View'

---
+++ Minimal-J/src/main/java/ch/openech/mj/model/annotation/ViewOf.java
@@ -0,0 +1,11 @@
+package ch.openech.mj.model.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ViewOf {
+
+	Class<?> value();
+	
+}
