01ca58cffc1f3a795c5aaafe2506131f23efd9b6
bruno <bruno.eberhard@pop.ch>
2014-04-07 20:32:11 +0200

    CustomerForm: made demoable

--- Minimal-J_Example/src/main/java/ch/openech/mj/example/CustomerForm.java
+++ Minimal-J_Example/src/main/java/ch/openech/mj/example/CustomerForm.java
@@ -1,6 +1,7 @@
 package ch.openech.mj.example;
 
 import static ch.openech.mj.example.model.Customer.*;
+import ch.openech.mj.autofill.NameGenerator;
 import ch.openech.mj.edit.form.Form;
 import ch.openech.mj.example.model.Customer;
 
@@ -14,6 +15,14 @@ public class CustomerForm extends Form<Customer> {
 		line(CUSTOMER.birthDay);
 		line(CUSTOMER.remarks);
 		
+	}
+
+	@Override
+	protected void fillWithDemoData(Customer customer) {
+		super.fillWithDemoData(customer);
+		customer.name = NameGenerator.officialName();
+		customer.firstName = "Dejan";
+		// customer.firstName = FirstNameGenerator.getFirstName(Math.random() < 0.5);
 	}
 	
 }
