bd510d31336a22fd83051e7a0223a12133ff2e48
bruno <bruno.eberhard@pop.ch>
2014-04-08 20:59:25 +0200

    Annotations: fixed Target

--- Minimal-J/src/main/java/ch/openech/mj/model/annotation/Code.java
+++ Minimal-J/src/main/java/ch/openech/mj/model/annotation/Code.java
@@ -1,7 +1,9 @@
 package ch.openech.mj.model.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Marks a property to be a code. Codes must be registred at the
@@ -9,6 +11,7 @@ import java.lang.annotation.RetentionPolicy;
  * 
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Code {
 
 	String value() default "";
