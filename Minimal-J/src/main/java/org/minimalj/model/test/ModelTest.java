03b28663f0d2284d8cdb7971a52622fe314c8ebd
bruno <bruno.eberhard@pop.ch>
2014-06-05 07:55:02 +0200

    ModelTest: id can also be byte or short

--- Minimal-J/src/main/java/org/minimalj/model/test/ModelTest.java
+++ Minimal-J/src/main/java/org/minimalj/model/test/ModelTest.java
@@ -91,8 +91,8 @@ public class ModelTest {
 		try {
 			Field fieldId = clazz.getField("id");
 			if (mainModelClasses.contains(clazz)) {
-				if (fieldId.getType() == Long.class || fieldId.getType() == Integer.class) {
-					problems.add(clazz.getName() + ": Domain classes ids must be of primitiv type (int or long)");
+				if (fieldId.getType() == Long.class || fieldId.getType() == Integer.class || fieldId.getType() == Short.class || fieldId.getType() == Byte.class) {
+					problems.add(clazz.getName() + ": Domain classes ids must be of primitiv type (byte, short, int or long)");
 				}
 				if (!FieldUtils.isPublic(fieldId)) {
 					problems.add(clazz.getName() + ": field id must be public");
