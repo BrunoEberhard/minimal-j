19ed11daef52cf772b681c33524e3830c3f20aa1
bruno <bruno.eberhard@pop.ch>
2014-06-16 12:31:24 +0200

    Transaction: JavaDoc added

--- Minimal-J/src/main/java/org/minimalj/transaction/Transaction.java
+++ Minimal-J/src/main/java/org/minimalj/transaction/Transaction.java
@@ -4,8 +4,27 @@ import java.io.Serializable;
 
 import org.minimalj.backend.Backend;
 
+/**
+ * The transaction is the action the frontend passes to the backend for
+ * execution.<p>
+ * 
+ * For simple CRUD-Transactions there exist shortcuts in the backend
+ * interface.<p>
+ *
+ * @param <T> type of return value. Has to be an extension of Serializable.
+ * Sadly <code>Void</code> is not serializable. Use Integer if you don't have a return value
+ * and return null from the execute method.
+ */
 public interface Transaction<T extends Serializable> extends Serializable {
 
+	/**
+	 * The invocation method for the backend. Application code should not need
+	 * to call this method directly.
+	 * 
+	 * @param backend if the transcation needs things from the Backend this
+	 * provides the access.
+	 * @return the return value from the transaction
+	 */
 	public T execute(Backend backend);
 	
 }
