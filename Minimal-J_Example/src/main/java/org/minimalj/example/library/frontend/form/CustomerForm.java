62218cff190ccb099cff4f8b2db39b8b9ed0aa6f
bruno <bruno.eberhard@pop.ch>
2014-06-24 06:44:18 +0200

    CustomerForm: reactivated FirstNameGenerator

--- Minimal-J_Example/src/main/java/org/minimalj/example/library/frontend/form/CustomerForm.java
+++ Minimal-J_Example/src/main/java/org/minimalj/example/library/frontend/form/CustomerForm.java
@@ -2,6 +2,7 @@ package org.minimalj.example.library.frontend.form;
 
 import static org.minimalj.example.library.model.Customer.*;
 
+import org.minimalj.autofill.FirstNameGenerator;
 import org.minimalj.autofill.NameGenerator;
 import org.minimalj.example.library.model.Customer;
 import org.minimalj.frontend.edit.form.Form;
@@ -22,8 +23,7 @@ public class CustomerForm extends Form<Customer> {
 	protected void fillWithDemoData(Customer customer) {
 		super.fillWithDemoData(customer);
 		customer.name = NameGenerator.officialName();
-		customer.firstName = "Dejan";
-		// customer.firstName = FirstNameGenerator.getFirstName(Math.random() < 0.5);
+		customer.firstName = FirstNameGenerator.getFirstName(Math.random() < 0.5);
 	}
 	
 }
