05c1c080531940fc6c6c24aa74769c778de832cd
bruno <bruno.eberhard@pop.ch>
2014-08-10 09:17:40 +0200

    StringUtils.escapeHTML: added missing & escaping

--- Minimal-J/src/main/java/org/minimalj/util/StringUtils.java
+++ Minimal-J/src/main/java/org/minimalj/util/StringUtils.java
@@ -140,7 +140,7 @@ public class StringUtils {
 	public static void escapeHTML(StringBuilder builder, String s) {
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
-			if (c > 127 || c == '"' || c == '<' || c == '>') {
+			if (c > 127 || c == '"' || c == '<' || c == '>' || c == '&') {
 				builder.append("&#");
 				builder.append((int) c);
 				builder.append(';');
