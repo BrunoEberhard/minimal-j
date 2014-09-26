c5f790551c98d2e92d580e5e020ab5f5e30817f3
bruno <bruno.eberhard@pop.ch>
2014-09-26 21:20:28 +0200

    IdUtils: cache fields for performance

--- Minimal-J/src/main/java/org/minimalj/util/IdUtils.java
+++ Minimal-J/src/main/java/org/minimalj/util/IdUtils.java
@@ -1,20 +1,40 @@
 package org.minimalj.util;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 
 public class IdUtils {
 	
 	private static final Logger logger = Logger.getLogger(IdUtils.class.getName());
-
+	private static final Map<Class<?>, Field> idFieldOfClass = new HashMap<>(200);
+	
+	private static Field getIdField(Class<?> clazz) {
+		if (idFieldOfClass.containsKey(clazz)) {
+			return idFieldOfClass.get(clazz);
+		}
+		try {
+			Field idField = clazz.getField("id");
+			idFieldOfClass.put(clazz, idField);
+			return idField;
+		} catch (NoSuchFieldException e) {
+			idFieldOfClass.put(clazz, null);
+			return null;
+		} catch (SecurityException e) {
+			throw new LoggingRuntimeException(e, logger, "getting Id failed");
+		}
+	}
+	
 	public static Object getId(Object object) {
 		if (object != null) {
 			try {
-				Field idField = object.getClass().getField("id");
+				Field idField = getIdField(object.getClass());
+				if (idField == null) throw new IllegalArgumentException(object.getClass().getName() + " has no id field to get");
 				Object id = idField.get(object);
 				return id;
-			} catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
+			} catch (SecurityException | IllegalAccessException e) {
 				throw new LoggingRuntimeException(e, logger, "getting Id failed");
 			}
 		} else {
@@ -22,14 +42,25 @@ public class IdUtils {
 		}
 	}
 	
+	public static void setIdSafe(Object object, Object id) {
+		try {
+			Field idField = getIdField(object.getClass());
+			if (idField != null) {
+				idField.set(object, id);
+			}
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			throw new LoggingRuntimeException(e, logger, "setting Id failed");
+		}
+	}
+	
 	public static void setId(Object object, Object id) {
 		try {
-			Field idField = object.getClass().getField("id");
-			idField.set(object, id);
-		} catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
-			// throw new LoggingRuntimeException(e, logger, "setting Id failed");
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
+			Field idField = getIdField(object.getClass());
+			if (idField != null) {
+				idField.set(object, id);
+			}
+		} catch (IllegalAccessException | IllegalArgumentException e) {
+			throw new LoggingRuntimeException(e, logger, "setting Id failed");
 		}
 	}
 
