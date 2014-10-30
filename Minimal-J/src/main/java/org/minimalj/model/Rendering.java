80f354faa51db7d6fe20dee3129280b66a449afb
bruno <bruno.eberhard@pop.ch>
2014-10-30 20:40:01 +0100

    Rendering: concept of defining what is displayed in a combobox for an object

---
+++ Minimal-J/src/main/java/org/minimalj/model/Rendering.java
@@ -0,0 +1,26 @@
+package org.minimalj.model;
+
+import java.util.Locale;
+
+public interface Rendering {
+
+	public static enum RenderType {
+		PLAIN_TEXT, HMTL;
+	}
+	
+	/**
+	 * Note: it's ok to ignore the render type or the locale.
+	 * If asked for HTML the answer can be a plain text. If the
+	 * application doesn't care about different languages you
+	 * can ignore the locale parameter completly.
+	 * 
+	 */
+	public String render(RenderType renderType, Locale locale);
+	
+	public static interface RenderingWithTooltip extends Rendering {
+		
+		public String renderTooltip(RenderType renderType, Locale locale);
+		
+	}
+	
+}
