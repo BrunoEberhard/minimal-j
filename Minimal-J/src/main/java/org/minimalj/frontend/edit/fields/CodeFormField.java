ed6b8bd45f4d30d7710461f47b41ecf65ec81fc0
bruno <bruno.eberhard@pop.ch>
2014-10-31 08:26:52 +0100

    CodeFormField: use rendering, not toString

--- Minimal-J/src/main/java/org/minimalj/frontend/edit/fields/CodeFormField.java
+++ Minimal-J/src/main/java/org/minimalj/frontend/edit/fields/CodeFormField.java
@@ -1,11 +1,14 @@
 package org.minimalj.frontend.edit.fields;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.minimalj.frontend.toolkit.ClientToolkit;
 import org.minimalj.frontend.toolkit.ClientToolkit.IComponent;
 import org.minimalj.frontend.toolkit.TextField;
 import org.minimalj.model.PropertyInterface;
+import org.minimalj.model.Rendering;
+import org.minimalj.model.Rendering.RenderType;
 import org.minimalj.model.annotation.Code;
 import org.minimalj.util.Codes;
 
@@ -14,6 +17,7 @@ public class CodeFormField<T extends Code> extends AbstractEditField<Object> {
 	private final List<T> codes;
 	private final TextField textFieldDisabled;
 
+	@SuppressWarnings("unchecked")
 	public CodeFormField(PropertyInterface property) {
 		super(property, true);
 		codes = Codes.get((Class<T>) property.getFieldClazz());
@@ -33,8 +37,16 @@ public class CodeFormField<T extends Code> extends AbstractEditField<Object> {
 
 	@Override
 	public void setObject(Object value) {
-		Object code = Codes.findCode(codes, value);
-		textFieldDisabled.setText(code != null ? code.toString() : null);
+		Code code = Codes.findCode(codes, value);
+		String text;
+		if (code instanceof Rendering) {
+			text = ((Rendering) code).render(RenderType.PLAIN_TEXT, Locale.getDefault());
+		} else if (code != null) {
+			text = code.toString();
+		} else {
+			text = null;
+		}
+		textFieldDisabled.setText(text);
 	}
 
 }
