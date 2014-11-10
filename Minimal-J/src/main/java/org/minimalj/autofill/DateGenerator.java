e13d9309c92d712135934b8923a3a9eb9daec1dd
bruno <bruno.eberhard@pop.ch>
2014-11-10 17:15:54 +0100

    DateGenerator: formatting

--- Minimal-J/src/main/java/org/minimalj/autofill/DateGenerator.java
+++ Minimal-J/src/main/java/org/minimalj/autofill/DateGenerator.java
@@ -7,49 +7,51 @@ import org.threeten.bp.LocalTime;
 public class DateGenerator {
 
 	public static LocalDate generateRandomDate() {
-		int year =(int)(Math.random() * 80) + 1930;
-		int month =(int)(Math.random() * 12) + 1;
+		int year = (int) (Math.random() * 80) + 1930;
+		int month = (int) (Math.random() * 12) + 1;
 		int day;
 		if (month == 4 || month == 6 || month == 9 || month == 11) {
-			day =(int)(Math.random() * 30) + 1;
+			day = (int) (Math.random() * 30) + 1;
 		} else if (month == 2) {
-			day =(int)(Math.random() * 28) + 1;
+			day = (int) (Math.random() * 28) + 1;
 		} else {
-			day =(int)(Math.random() * 31) + 1;
+			day = (int) (Math.random() * 31) + 1;
 		}
 		LocalDate localDate = LocalDate.of(year, month, day);
 		return localDate;
 	}
-	
-	public static String generateRandomDatePartiallyKnown() {
-		int year = (int)(Math.random() * 80) + 1930;
-		if (Math.random() > 0.98) return "" + year;
 
-		int month = (int)(Math.random() * 12) + 1;
-		if (Math.random() > 0.98) return year + "-" + month;
-		
+	public static String generateRandomDatePartiallyKnown() {
+		int year = (int) (Math.random() * 80) + 1930;
+		if (Math.random() > 0.98)
+			return "" + year;
+
+		int month = (int) (Math.random() * 12) + 1;
+		if (Math.random() > 0.98)
+			return year + "-" + month;
+
 		int day;
 		if (month == 4 || month == 6 || month == 9 || month == 11) {
-			day =(int)(Math.random() * 30) + 1;
+			day = (int) (Math.random() * 30) + 1;
 		} else if (month == 2) {
-			day =(int)(Math.random() * 28) + 1;
+			day = (int) (Math.random() * 28) + 1;
 		} else {
-			day =(int)(Math.random() * 31) + 1;
+			day = (int) (Math.random() * 31) + 1;
 		}
 		return year + "-" + month + "-" + day;
 	}
-	
+
 	public static LocalTime generateRandomTime(int size) {
-		int hour =(int)(Math.random() * 24);
-		int minute =(int)(Math.random() * 60);
+		int hour = (int) (Math.random() * 24);
+		int minute = (int) (Math.random() * 60);
 		if (size == Size.TIME_HH_MM) {
 			return LocalTime.of(hour, minute);
 		} else {
-			int second =(int)(Math.random() * 60);
+			int second = (int) (Math.random() * 60);
 			if (size == Size.TIME_WITH_SECONDS) {
 				return LocalTime.of(hour, minute, second);
 			} else if (size == Size.TIME_WITH_SECONDS) {
-				int milis =(int)(Math.random() * 1000);
+				int milis = (int) (Math.random() * 1000);
 				return LocalTime.of(hour, minute, second, milis);
 			} else {
 				throw new IllegalArgumentException(String.valueOf(size));
