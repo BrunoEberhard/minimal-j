5ed3cf060fd1f9aa4308ceb897ea562e8dd26447
bruno <bruno.eberhard@pop.ch>
2014-12-07 11:37:47 +0100

    PropertyInterface: JavaDoc

--- Minimal-J/src/main/java/org/minimalj/model/properties/PropertyInterface.java
+++ Minimal-J/src/main/java/org/minimalj/model/properties/PropertyInterface.java
@@ -4,10 +4,15 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
 /**
- * Somehow the common interface of a public field and
- * a getter/setter pair
+ * Properties can be public fields or getter/setter pairs. This interface is intended
+ * for internal Minimal-J use. Sometimes it's also usefull for an actual application but
+ * you can use Minimal-J without furthur knowledge of this interface and its implementations.<p>
  * 
- * @author bruno
+ * Some properties can be chained. You can as the Keys class for a Property "propertyA.propertyB".
+ * This means you get the propertyB of the class used for propertyA. This chains are the
+ * reason for the getPath method in this interface. For all other method the last part
+ * of the chain is used for ChainedProperties.<p>
+ * 
  */
 public interface PropertyInterface {
 
