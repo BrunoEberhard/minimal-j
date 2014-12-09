c431e82725d2b43dcee33b6ac970b0f74e0a8b46
bruno <bruno.eberhard@pop.ch>
2014-12-09 07:57:39 +0100

    First two examples added

---
+++ examples/Example_001_EmptyApplication/src/org/minimalj/example/empty/EmptyApplication.java
@@ -0,0 +1,7 @@
+package org.minimalj.example.empty;
+
+import org.minimalj.application.Application;
+
+public class EmptyApplication extends Application {
+
+}
---
+++ examples/Example_002_HelloWorld/src/org/minimalj/example/helloworld/HelloWorldPage.java
@@ -0,0 +1,27 @@
+package org.minimalj.example.helloworld;
+
+import org.minimalj.frontend.page.Page;
+import org.minimalj.frontend.toolkit.ClientToolkit;
+import org.minimalj.frontend.toolkit.ClientToolkit.IContent;
+import org.minimalj.frontend.toolkit.FormContent;
+
+public class HelloWorldPage implements Page {
+
+	@Override
+	public String getTitle() {
+		return "Hello World";
+	}
+
+	@Override
+	public IContent getContent() {
+		FormContent form = ClientToolkit.getToolkit().createFormContent(1, 100);
+		form.add(ClientToolkit.getToolkit().createLabel("Hello World"));
+		return form;
+	}
+
+	@Override
+	public void refresh() {
+		// nothing to do
+	}
+
+}
