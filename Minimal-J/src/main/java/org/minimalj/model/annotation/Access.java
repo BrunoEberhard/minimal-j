cd3db196876ecb5d6f46bdc92a12713d930676f9
bruno <bruno.eberhard@pop.ch>
2014-12-15 07:56:22 +0100

    Access: JavaDoc

--- Minimal-J/src/main/java/org/minimalj/model/annotation/Access.java
+++ Minimal-J/src/main/java/org/minimalj/model/annotation/Access.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation specifies the needed role to access a field or
+ * a transaction class.<p>
+ * 
+ * NOT YET IMPLEMENTED
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Access {
