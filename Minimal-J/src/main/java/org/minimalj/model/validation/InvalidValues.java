b38497a9362d7d93834b8e36ae3c4ef238b63852
bruno <bruno.eberhard@pop.ch>
2014-12-18 07:44:58 +0100

    InvalidValues: create more than one objects of one class of invalid values would have failed. Fixed this and added some tests

--- Minimal-J/src/main/java/org/minimalj/model/validation/InvalidValues.java
+++ Minimal-J/src/main/java/org/minimalj/model/validation/InvalidValues.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.util.Map;
 import java.util.WeakHashMap;
@@ -15,7 +16,7 @@ import org.minimalj.model.EnumUtils;
  * as defined by their field classes.<p>
  * 
  * For example '42A' cannot be converted to an Integer. But temporarly it should
- * be stored in an Integer field because it's entered in a form field.<p>
+ * be stored in an Integer field because it's entered by the user.<p>
  * 
  * The form class converts the String in an InvalidValues object. So the
  * value can be stored in the Integer field. All validators should check for
@@ -27,11 +28,25 @@ import org.minimalj.model.EnumUtils;
 public class InvalidValues {
 
 	private static final Map<Object, String> values = new WeakHashMap<Object, String>();
+	private static int counter = Integer.MIN_VALUE + 1;
+	private static LocalDate MIN_DATE = LocalDate.of(0, 1, 1);
+	private static LocalTime MIN_TIME = LocalTime.of(0, 0, 0);
+	private static LocalDateTime MIN_DATETIME = LocalDateTime.of(0, 1, 1, 0, 0, 0);
 	
+	/**
+	 * @return true if the value is an object created previously with one of the
+	 * createInvalid methods
+	 */
 	public static boolean isInvalid(Object value) {
 		return values.containsKey(value);
 	}
-	
+
+	/**
+	 * @param value a value with one of the createInvalid methods
+	 * @return the string represented by this invalid value
+	 * @throws IllegalArgumentException if the value wasn't created by one of the
+	 * createInvalid methods
+	 */
  	public static String getInvalidValue(Object value) {
 		if (values.containsKey(value)) {
 			return values.get(value);
@@ -41,55 +56,55 @@ public class InvalidValues {
 	}
 
 	public static String createInvalidString(String string) {
-		String s = new String(); // dont change
+		String s = new String("INVALID" + (counter++)); // dont change
 		values.put(s, string);
 		return s;
 	}
 	
 	public static Integer createInvalidInteger(String string) {
-		Integer key = new Integer(Integer.MAX_VALUE);
+		Integer key = new Integer(counter++);
 		values.put(key, string);
 		return key;
 	}
 	
 	public static Long createInvalidLong(String string) {
-		Long key = new Long(Long.MAX_VALUE);
+		Long key = new Long(counter++);
 		values.put(key, string);
 		return key;
 	}
 	
 	public static <T extends Enum<T>> T createInvalidEnum(Class<T> enumClass, String value) {
-		T e = EnumUtils.createEnum(enumClass, "INVALID");
+		T e = EnumUtils.createEnum(enumClass, "INVALID" + (counter++));
 		values.put(e, value);
 		return e;
 	}
 
 	public static Temporal createInvalidPartial(String string) {
-		Temporal partial = LocalDate.now();
+		Temporal partial = MIN_DATE.plus(counter++, ChronoUnit.DAYS);
 		values.put(partial, string);
 		return partial;
 	}
 
 	public static LocalDate createInvalidLocalDate(String string) {
-		LocalDate localDate = LocalDate.now();
+		LocalDate localDate = MIN_DATE.plus(counter++, ChronoUnit.DAYS);
 		values.put(localDate, string);
 		return localDate;
 	}
 
 	public static LocalDateTime createInvalidLocalDateTime(String string) {
-		LocalDateTime localDateTime = LocalDateTime.now();
+		LocalDateTime localDateTime = MIN_DATETIME.plus(counter++, ChronoUnit.DAYS);
 		values.put(localDateTime, string);
 		return localDateTime;
 	}
 	
 	public static LocalTime createInvalidLocalTime(String string) {
-		LocalTime localTime = LocalTime.now();
+		LocalTime localTime = MIN_TIME.plus(counter++, ChronoUnit.MILLIS);
 		values.put(localTime, string);
 		return localTime;
 	}
 	
 	public static BigDecimal createInvalidBigDecimal(String string) {
-		BigDecimal bigDecimal = new BigDecimal(0);
+		BigDecimal bigDecimal = new BigDecimal(counter++);
 		values.put(bigDecimal, string);
 		return bigDecimal;
 	}
