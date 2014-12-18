a2fd5b5ebd464a2b9eba967a1d4619f4aa95c1fa
bruno <bruno.eberhard@pop.ch>
2014-12-18 07:51:33 +0100

    SubTable: fixed java doc

--- Minimal-J/src/main/java/org/minimalj/backend/db/SubTable.java
+++ Minimal-J/src/main/java/org/minimalj/backend/db/SubTable.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.minimalj.model.properties.PropertyInterface;
 
 /**
- * Minimal-J internal<p>
+ * Minimal-J internal
  * 
  */
 @SuppressWarnings({"rawtypes", "unchecked"})
