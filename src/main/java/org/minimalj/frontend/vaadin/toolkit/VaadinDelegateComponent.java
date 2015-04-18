916be6d31885d54f1ccdbce1f5aabe0f0013d0a7
bruno <bruno.eberhard@pop.ch>
2015-04-18 08:49:57 +0200

    ClientToolkit: merged CheckBox, ComboBox, ILookup in Input

---
+++ src/main/java/org/minimalj/frontend/vaadin/toolkit/VaadinDelegateComponent.java
@@ -0,0 +1,17 @@
+package org.minimalj.frontend.vaadin.toolkit;
+
+import org.minimalj.frontend.toolkit.ClientToolkit.IComponent;
+
+import com.vaadin.ui.Component;
+
+/**
+ * The vaadin text components have a problem to implement Input<String> because
+ * of a name clash in getValue method. This delegate is a work around to this.<p>
+ * 
+ * This is acceptable as Vaadin 6 will soon be deprecated as FrontEnd
+ */
+public interface VaadinDelegateComponent extends IComponent {
+
+	public Component getDelegate();
+	
+}
