22fc75c2c11840dfe162d7b667ebbc8a3a201ba3
bruno <bruno.eberhard@pop.ch>
2015-04-22 17:38:35 +0200

    TextField: cleanup

--- src/main/java/org/minimalj/frontend/toolkit/TextField.java
+++ src/main/java/org/minimalj/frontend/toolkit/TextField.java
@@ -3,22 +3,7 @@ package org.minimalj.frontend.toolkit;
 import org.minimalj.frontend.toolkit.ClientToolkit.Input;
 
 
-
 public interface TextField extends Input<String> {
-	
-//	public default void setValue(Object value) {
-//		setValue((String) value);
-//	}
-//	
-//	public default String getValue() {
-//		return (String) getValue();
-//	}
-//	
-//	public void setValue(String text);
-//
-//	public String getValue();
-
-	public void setEditable(boolean editable);
 	
 	public void setFocusListener(IFocusListener focusListener);
 	
