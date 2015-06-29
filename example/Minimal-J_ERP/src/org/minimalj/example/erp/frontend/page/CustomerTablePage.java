545f0f349f66789d7423757221a05fec37c24dfa
bruno <bruno.eberhard@pop.ch>
2015-06-29 19:15:46 +0200

    Pages: TablePageWithDetail

--- example/Minimal-J_ERP/src/org/minimalj/example/erp/frontend/page/CustomerTablePage.java
+++ example/Minimal-J_ERP/src/org/minimalj/example/erp/frontend/page/CustomerTablePage.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.minimalj.backend.Backend;
 import org.minimalj.example.erp.model.Customer;
-import org.minimalj.frontend.page.TablePage;
-import org.minimalj.frontend.toolkit.ClientToolkit;
+import org.minimalj.frontend.page.ObjectPage;
+import org.minimalj.frontend.page.TablePage.SimpleTablePageWithDetail;
 import org.minimalj.transaction.criteria.Criteria;
 
 
-public class CustomerTablePage extends TablePage<Customer> {
+public class CustomerTablePage extends SimpleTablePageWithDetail<Customer> {
 
 	public CustomerTablePage() {
 		super(CustomerSearchPage.FIELDS);
@@ -21,8 +21,8 @@ public class CustomerTablePage extends TablePage<Customer> {
 	}
 
 	@Override
-	public void action(Customer selectedObject) {
-		ClientToolkit.getToolkit().show(new CustomerPage(selectedObject));
+	protected ObjectPage<Customer> createPage(Customer customer) {
+		return new CustomerPage(customer);
 	}
 
 }
