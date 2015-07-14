4d1feb6198d1f6bac3816dece0a12674718ecb82
bruno <bruno.eberhard@pop.ch>
2015-07-14 10:17:11 +0200

    VaadinDelegateComponent: fix javadoc error

--- src/main/java/org/minimalj/frontend/vaadin/toolkit/VaadinDelegateComponent.java
+++ src/main/java/org/minimalj/frontend/vaadin/toolkit/VaadinDelegateComponent.java
@@ -5,7 +5,7 @@ import org.minimalj.frontend.toolkit.ClientToolkit.IComponent;
 import com.vaadin.ui.Component;
 
 /**
- * The vaadin text components have a problem to implement Input<String> because
+ * The vaadin text components have a problem to implement Input&lt;String&gt; because
  * of a name clash in getValue method. This delegate is a work around to this.<p>
  * 
  * This is acceptable as Vaadin 6 will soon be deprecated as FrontEnd
