1c08a9bf14d83cb9bf386a8cc9baceca08b3c8db
bruno <bruno.eberhard@pop.ch>
2015-08-21 10:57:25 +0200

    Size Annotation must be usable for methods (for method - properties)

--- src/main/java/org/minimalj/model/annotation/Size.java
+++ src/main/java/org/minimalj/model/annotation/Size.java
@@ -17,7 +17,7 @@ import java.lang.annotation.Target;
  * 
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface Size {
 
 	int value();
