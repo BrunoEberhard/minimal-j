b2658cf6218ddeec402013a0c59bdc8ebc315bfc
bruno <bruno.eberhard@pop.ch>
2015-09-05 09:39:32 +0200

    Nano Client: also do placeholder replacement

--- src/main/java/org/minimalj/frontend/impl/nanoserver/MjWebSocketServer.java
+++ src/main/java/org/minimalj/frontend/impl/nanoserver/MjWebSocketServer.java
@@ -1,19 +1,26 @@
 package org.minimalj.frontend.impl.nanoserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.minimalj.frontend.impl.json.JsonHandler;
-import org.minimalj.frontend.impl.nanoserver.httpd.NanoWebSocketServer;
 import org.minimalj.frontend.impl.nanoserver.httpd.NanoHTTPD.Response.Status;
+import org.minimalj.frontend.impl.nanoserver.httpd.NanoWebSocketServer;
 import org.minimalj.frontend.impl.nanoserver.httpd.NanoWebSocketServer.WebSocketFrame.CloseCode;
+import org.minimalj.frontend.impl.servlet.MjServlet;
 import org.minimalj.util.resources.Resources;
 
 public class MjWebSocketServer extends NanoWebSocketServer {
 	private static final Logger logger = Logger.getLogger(MjWebSocketServer.class.getName());
 
+	private static String htmlTemplate;
+	
 	private JsonHandler handler = new JsonHandler();
 	
 	public MjWebSocketServer(int port, boolean secure) {
@@ -29,12 +36,17 @@ public class MjWebSocketServer extends NanoWebSocketServer {
 			}
 		}
 	}
+	
+	static {
+		htmlTemplate = readStream(MjServlet.class.getClassLoader().getResourceAsStream("index.html"));
+	}
 
 	@Override
     public Response serve(String uri, Method method, Map<String, String> headers, Map<String, String> parms,
             Map<String, String> files) {
 		if (uri.equals("/")) {
-			return newChunkedResponse(Status.OK, "text/html", this.getClass().getClassLoader().getResourceAsStream("index.html"));
+			String html = fillPlaceHolder(htmlTemplate, "de"); // headers.get("accept-language")
+			return newFixedLengthResponse(Status.OK, "text/html", html);
 		} else if (uri.equals("/mj.css")) {
 			return newChunkedResponse(Status.OK, "text/css", this.getClass().getClassLoader().getResourceAsStream("mj.css"));
 
@@ -76,6 +88,22 @@ public class MjWebSocketServer extends NanoWebSocketServer {
 	@Override
 	protected void onException(WebSocket webSocket, IOException e) {
 		// TODO Auto-generated method stub
+	}
+	
+	// TODO merge with MjServlet
+	private static String readStream(InputStream inputStream) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		return reader.lines().collect(Collectors.joining(System.getProperty("line.separator")));
+	}
+	
+	// TODO merge with MjServlet
+	protected String fillPlaceHolder(String htmlTemplate, String locale) {
+		String result = htmlTemplate.replace("$LOCALE", locale);
+		result = result.replace("$FORCE_WSS", "false");
+		result = result.replace("$PORT", "");
+		result = result.replace("$WS", "ws");
+		result = result.replace("$SEARCH", Resources.getString("SearchAction"));
+		return result;
 	}
 
 }
