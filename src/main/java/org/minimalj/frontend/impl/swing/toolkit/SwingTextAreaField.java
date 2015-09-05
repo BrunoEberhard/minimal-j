19e1b3144fd9b2bdcb5678fb941d418cb4d8a942
bruno <bruno.eberhard@pop.ch>
2015-09-05 11:14:53 +0200

    SwingTextAreaField: show multiple rows, correct border and font

--- src/main/java/org/minimalj/frontend/impl/swing/toolkit/SwingTextAreaField.java
+++ src/main/java/org/minimalj/frontend/impl/swing/toolkit/SwingTextAreaField.java
@@ -1,21 +1,30 @@
 package org.minimalj.frontend.impl.swing.toolkit;
 
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 import org.minimalj.frontend.Frontend.Input;
 import org.minimalj.frontend.Frontend.InputComponentListener;
 
-public class SwingTextAreaField extends JTextArea implements Input<String> {
+public class SwingTextAreaField extends JScrollPane implements Input<String> {
+	private static final long serialVersionUID = 1L;
 	
 	private final InputComponentListener changeListener;
+	private final JTextArea textArea;
 	
 	public SwingTextAreaField(InputComponentListener changeListener, int maxLength, String pattern) {
-		super(new SwingTextField.FilteredDocument(maxLength, pattern));
+		textArea = new JTextArea(new SwingTextField.FilteredDocument(maxLength, pattern));
+		textArea.setFont(UIManager.getDefaults().getFont("TextField.font"));
+		setBorder(UIManager.getDefaults().getBorder("TextField.border"));
+		textArea.setLineWrap(true);
+		textArea.setRows(calcRows(maxLength));
+		textArea.getDocument().addDocumentListener(new TextFieldChangeListener());
+		setViewportView(textArea);
 		
 		this.changeListener = changeListener;
-		getDocument().addDocumentListener(new TextFieldChangeListener());
 		
 		// not yet supported
 //		textArea.addActionListener(new ActionListener() {
@@ -26,6 +35,16 @@ public class SwingTextAreaField extends JTextArea implements Input<String> {
 //				}
 //			}
 //		});
+	}
+	
+	// TODO make number of rows in SwingTextAreaField somehow configurable
+	private int calcRows(int maxLength) {
+		int rows = 1;
+		while (maxLength > 1) {
+			rows++;
+			maxLength = maxLength / 3;
+		}
+		return rows;
 	}
 
 	public class TextFieldChangeListener implements DocumentListener {
@@ -52,12 +71,17 @@ public class SwingTextAreaField extends JTextArea implements Input<String> {
 
 	@Override
 	public void setValue(String value) {
-		super.setText(value);
+		textArea.setText(value);
 	}
 
 	@Override
 	public String getValue() {
-		return super.getText();
+		return textArea.getText();
+	}
+
+	@Override
+	public void setEditable(boolean editable) {
+		textArea.setEditable(editable);
 	}
 }
 
