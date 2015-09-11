aec5dfd1197e1fd7f0fa9dba2f92076458468d5f
bruno <bruno.eberhard@pop.ch>
2015-09-11 08:42:32 +0200

    Vaadin: package-info

---
+++ src/main/java/org/minimalj/frontend/impl/vaadin6/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Vaadin was used before the Json Frontend. It gets more and more outdated
+ * as I don't implement the changes anymore. I also don't have the time to check
+ * everytime if the old stuff is still working.<p>
+ * 
+ * Missing parts:
+ * <UL>
+ * <LI>The navigation is still a menu. Should be a pane on the left as in Json Frontend</LI>
+ * <LI>Vaadin Frontend shows only one page at a time instead of show detail pages below master pages</LI>
+ * <LI>Html content</LI>
+ * </UL>
+ */
+
+@java.lang.Deprecated
+package org.minimalj.frontend.impl.vaadin6;
