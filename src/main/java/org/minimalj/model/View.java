c2ee64ce3c6394e63d8c8d7d0a38e1f0c7f4ffde
bruno <bruno.eberhard@pop.ch>
2015-09-11 14:24:47 +0200

    View: added javadoc

--- src/main/java/org/minimalj/model/View.java
+++ src/main/java/org/minimalj/model/View.java
@@ -3,10 +3,14 @@ package org.minimalj.model;
 
 /**
  * A class implementing View is a model class that holds only a part
- * of the fields of an other class. 
+ * of the fields of an other class.<p>
  * 
  * The objects of this class are never saved if they referenced
- * by other objects
+ * by other objects.<p>
+ * 
+ * Note that while the concept is similar to database views this
+ * interface does <i>not</i> represent views on the database but
+ * views on a java model entity.
  * 
  * @param <T> the class on which this view is based. Mandatory.
  */
