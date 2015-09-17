b625a5c000871242e2d9b82a1a0c1d69772ae272
bruno <bruno.eberhard@pop.ch>
2015-09-17 12:15:12 +0200

    HtmlPage: added stream to string conversion util method

--- src/main/java/org/minimalj/frontend/page/HtmlPage.java
+++ src/main/java/org/minimalj/frontend/page/HtmlPage.java
@@ -1,5 +1,11 @@
 package org.minimalj.frontend.page;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.Frontend.IContent;
 
@@ -11,9 +17,20 @@ public class HtmlPage extends Page {
 		this.htmlOrUrl = htmlOrUrl;
 	}
 	
+	public HtmlPage(InputStream inputStream) {
+		this(readStream(inputStream));
+	}
+	
 	@Override
 	public IContent getContent() {
 		return Frontend.getInstance().createHtmlContent(htmlOrUrl);
 	}
 
+	public static String readStream(InputStream inputStream) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+			return reader.lines().collect(Collectors.joining(System.getProperty("line.separator")));
+		} catch (IOException x) {
+			throw new RuntimeException(x);
+		}
+	}
 }
