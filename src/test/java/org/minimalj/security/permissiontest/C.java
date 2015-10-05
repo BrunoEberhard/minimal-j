ff926cbe9073cd3fea9897bf6ba282cf54486491
bruno <bruno.eberhard@pop.ch>
2015-10-05 16:37:04 +0200

    authorization.adoc: some first lines about security

--- src/test/java/org/minimalj/security/permissiontest/C.java
+++ src/test/java/org/minimalj/security/permissiontest/C.java
@@ -3,7 +3,7 @@ package org.minimalj.security.permissiontest;
 import org.minimalj.transaction.Role;
 import org.minimalj.transaction.Transaction.TransactionType;
 
-@Role(transaction = TransactionType.READ, value = {"ReadRole"})
+@Role(value = "ReadRole")
 @Role(transaction = TransactionType.UPDATE, value = {"UpdateRole"})
 public class C {
 
