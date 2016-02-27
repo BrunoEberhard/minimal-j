58999fcd88a533b7a5769785e70a2c03fa933c99
bruno <bruno.eberhard@pop.ch>
2016-02-27 15:53:54 +0100

    MjWebSocketDaemon: make keystore configurable

--- src/main/java/org/minimalj/frontend/impl/nanoserver/MjWebSocketDaemon.java
+++ src/main/java/org/minimalj/frontend/impl/nanoserver/MjWebSocketDaemon.java
@@ -26,14 +26,30 @@ public class MjWebSocketDaemon extends NanoWSD {
 		super(port);
 		if (secure) {
 			try {
+				// nanohttpd geht davon aus, dass keypass = storepass... ob das so sein darf / kann?
+				// bei storepass dürfte nanohttpd auch null mitgeben um die Überprüfung auszulassen...
+				
+				// note 1: to first read the property MjKeystorePassphrase and then convert it to char[]
+				// makes the whole char[] story senseless. But how to do it else? Maybe specify a filename
+				// and then read it byte by byte.
+				
+				// note 2: nanohttpd implies that keypass and storepass are the same passwords. I don't
+				// know if this is a good idea.
+				
+				// note 3: example to generate the store (todo: move to documentation)
 				// keytool.exe -keystore mjdevkeystore.jks -keyalg RSA -keysize 3072 -genkeypair -dname "cn=localhost, ou=MJ, o=Minimal-J, c=CH" -storepass mjdev1 -keypass mjdev1
 				// keytool.exe -keystore mjdevkeystore.jks -storepass mjdev1 -keypass mjdev1 -export -file mj.cer
-				makeSecure(makeSSLSocketFactory("/mjdevkeystore.jks", "mjdev1".toCharArray()), null);
+
+				String keyAndTrustStoreClasspathPath = System.getProperty("MjKeystore"); // in example '/mjdevkeystore.jks'
+				char[] passphrase = System.getProperty("MjKeystorePassphrase").toCharArray(); //  ub example 'mjdev1'
+				
+				makeSecure(makeSSLSocketFactory(keyAndTrustStoreClasspathPath, passphrase), null);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
+
 	
 	@Override
 	protected WebSocket openWebSocket(IHTTPSession handshake) {
