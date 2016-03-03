df996188e06a0bab77918a5dc96cea074b87d892
bruno <bruno.eberhard@pop.ch>
2016-03-03 17:05:06 +0100

    NanoHttpdApplication: don't use WebSocket Daemon if WS not used

--- src/main/java/org/minimalj/frontend/impl/nanoserver/NanoHttpdApplication.java
+++ src/main/java/org/minimalj/frontend/impl/nanoserver/NanoHttpdApplication.java
@@ -7,20 +7,24 @@ import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.impl.json.JsonFrontend;
 import org.minimalj.util.StringUtils;
 
+import fi.iki.elonen.NanoHTTPD;
+
 public class NanoHttpdApplication {
 	private static final boolean SECURE = true;
 	private static final int TIME_OUT = 5 * 60 * 1000;
+	
+	private static boolean useWebSocket = Boolean.valueOf(System.getProperty("MjUseWebSocket", "false"));
 	
 	private static int getPort(boolean secure) {
 		String portString = System.getProperty("MjFrontendPort" + (secure ? "Ssl" : ""), secure ? "-1" : "8080");
 		return !StringUtils.isEmpty(portString) ? Integer.valueOf(portString) : -1 ;
 	}
 	
-	private static MjWebSocketDaemon start(boolean secure) throws IOException {
+	private static NanoHTTPD start(boolean secure) throws IOException {
 		int port = getPort(secure);
 		if (port > 0) {
-			System.out.println("Start web frontend on " + port + (secure ? " (Secure)" : ""));
-			MjWebSocketDaemon daemon = new MjWebSocketDaemon(port, secure);
+			System.out.println("Start web " + (useWebSocket ? "socket" : "") + " frontend on " + port + (secure ? " (Secure)" : ""));
+			NanoHTTPD daemon = useWebSocket ? new MjWebSocketDaemon(port, secure) : new MjWebDaemon(port, secure);
 			daemon.start(TIME_OUT);
 			return daemon;
 		} else {
@@ -28,18 +32,22 @@ public class NanoHttpdApplication {
 		}
 	}
 	
-	private static void stop(MjWebSocketDaemon daemon) {
+	private static void stop(NanoHTTPD daemon) {
 		if (daemon != null) {
 			System.out.println("Stop web frontend on " + daemon.getListeningPort());
 			daemon.stop();
 		}
 	}
 	
+	public static boolean useWebSocket() {
+		return useWebSocket;
+	}
+	
 	public static void main(final String[] args) throws Exception {
 		Frontend.setInstance(new JsonFrontend());
 		Application.initApplication(args);
 		
-		MjWebSocketDaemon daemon = null, secureDaemon = null;
+		NanoHTTPD daemon = null, secureDaemon = null;
         try {
         	daemon = start(!SECURE);
         	secureDaemon = start(SECURE);
