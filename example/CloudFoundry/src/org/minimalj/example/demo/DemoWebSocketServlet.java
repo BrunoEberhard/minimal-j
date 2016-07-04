b5abe3c7ef7a89bb8cd84b61759723c8b5666f5f
bruno <bruno.eberhard@pop.ch>
2016-07-04 09:26:22 +0200

    Server redesign: use http or websocket for session management

---
+++ example/CloudFoundry/src/org/minimalj/example/demo/DemoWebSocketServlet.java
@@ -0,0 +1,55 @@
+package org.minimalj.example.demo;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.minimalj.frontend.impl.json.JsonPageManager;
+
+@ServerEndpoint(value = "/demoWs/{application}")
+public class DemoWebSocketServlet {
+	private static final Logger logger = Logger.getLogger(DemoWebSocketServlet.class.getName());
+
+	private Map<Session, JsonPageManager> pageManagers = new HashMap<>();
+	
+    public DemoWebSocketServlet() {
+    }
+
+    @OnOpen
+    public void start(Session session) {
+    	pageManagers.put(session, new JsonPageManager());
+    }
+
+    @OnClose
+    public void end(Session session) {
+    	pageManagers.remove(session);
+    }
+
+    @OnMessage
+    public void incoming(@PathParam("application") String applicationName, String message, Session session) {
+    	DemoServlet.examplesApplication.setCurrentApplication(applicationName);
+    	
+    	JsonPageManager pageManager = pageManagers.get(session);
+    	String result = pageManager.handle(message);
+    	try{
+    		session.getBasicRemote().sendText(result);
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, "Send response failed", e);
+		}
+    }
+
+    @OnError
+    public void onError(Throwable t) throws Throwable {
+        logger.severe("Error: " + t.toString());
+    }
+}
