d7d1927ca936e48851259ad03d0a6e94286d8acf
bruno <bruno.eberhard@pop.ch>
2016-11-06 14:14:28 +0100

    Vaadin deployed as springboot application

---
+++ ext/vaadin_springboot/src/main/java/org/minimalj/frontend/impl/vaadin/MjVaadinSpringbootApplication.java
@@ -0,0 +1,19 @@
+package org.minimalj.frontend.impl.vaadin;
+
+import org.minimalj.application.Application;
+import org.minimalj.frontend.Frontend;
+import org.minimalj.frontend.impl.vaadin.toolkit.VaadinFrontend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjVaadinSpringbootApplication {
+
+	public static void main(String[] args) {
+		Application.initApplication(args);
+		Frontend.setInstance(new VaadinFrontend());
+		
+		SpringApplication.run(MjVaadinSpringbootApplication.class, args);
+	}
+	
+}
---
+++ ext/vaadin_springboot/src/main/java/org/minimalj/frontend/impl/vaadin/VaadinSpringUI.java
@@ -0,0 +1,9 @@
+package org.minimalj.frontend.impl.vaadin;
+
+import com.vaadin.spring.annotation.SpringUI;
+
+@SpringUI
+public class VaadinSpringUI extends Vaadin {
+	private static final long serialVersionUID = 1L;
+
+}
