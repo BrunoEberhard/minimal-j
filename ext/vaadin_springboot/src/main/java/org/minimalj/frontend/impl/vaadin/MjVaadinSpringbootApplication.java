4c79d408dca5d40329f578c349abfea528ccc4bf
bruno <bruno.eberhard@pop.ch>
2016-11-11 07:42:50 +0100

    MjVaadinSpringbootApplication: fix main parameters

--- ext/vaadin_springboot/src/main/java/org/minimalj/frontend/impl/vaadin/MjVaadinSpringbootApplication.java
+++ ext/vaadin_springboot/src/main/java/org/minimalj/frontend/impl/vaadin/MjVaadinSpringbootApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MjVaadinSpringbootApplication {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		Application.initApplication(args);
 		Frontend.setInstance(new VaadinFrontend());
 		
