ca21726f3a97a1401fbb8be4df9d8c4e53033742
bruno <bruno.eberhard@pop.ch>
2016-11-30 17:59:32 +0100

    UserPassword: must be serializable

--- src/main/java/org/minimalj/security/model/UserPassword.java
+++ src/main/java/org/minimalj/security/model/UserPassword.java
@@ -1,10 +1,14 @@
 package org.minimalj.security.model;
 
+import java.io.Serializable;
+
 import org.minimalj.model.Keys;
 import org.minimalj.model.annotation.NotEmpty;
 import org.minimalj.model.annotation.Size;
 
-public class UserPassword {
+public class UserPassword implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	public static final UserPassword $ = Keys.of(UserPassword.class);
 
 	@Size(255) @NotEmpty
