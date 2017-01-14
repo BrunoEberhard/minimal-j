24c70144f56d80f34745276820ec065b76f88417
bruno <bruno.eberhard@pop.ch>
2017-01-14 14:27:29 +0100

    Isolation added

---
+++ src/main/java/org/minimalj/transaction/Isolation.java
@@ -0,0 +1,28 @@
+package org.minimalj.transaction;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Isolation {
+
+	Level value();
+
+	public static enum Level {
+		NONE(0), READ_UNCOMMITTED(1), READ_COMMITTED(2), REPEATABLE_READ(4), SERIALIZABLE(8);
+		
+		private final int level;
+		
+		private Level(int level) {
+			this.level = level;
+		}
+		
+		public int getLevel() {
+			return level;
+		}
+	}
+
+}
