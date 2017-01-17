b876d8ad3cea07f21f2cf88ccc02d1d40ec3d7b5
bruno <bruno.eberhard@pop.ch>
2017-01-17 08:08:19 +0100

    Isolation: use JDK constants

--- src/main/java/org/minimalj/transaction/Isolation.java
+++ src/main/java/org/minimalj/transaction/Isolation.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.sql.Connection;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.PACKAGE})
@@ -12,7 +13,8 @@ public @interface Isolation {
 	Level value();
 
 	public static enum Level {
-		NONE(0), READ_UNCOMMITTED(1), READ_COMMITTED(2), REPEATABLE_READ(4), SERIALIZABLE(8);
+		NONE(Connection.TRANSACTION_NONE), READ_UNCOMMITTED(Connection.TRANSACTION_READ_UNCOMMITTED),
+		READ_COMMITTED(Connection.TRANSACTION_READ_COMMITTED), REPEATABLE_READ(Connection.TRANSACTION_REPEATABLE_READ), SERIALIZABLE(Connection.TRANSACTION_SERIALIZABLE);
 		
 		private final int level;
 		
