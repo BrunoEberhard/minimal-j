bc260c9f4dc191e0aa55af1033fe20e5680af78f
bruno <bruno.eberhard@pop.ch>
2017-01-17 08:21:12 +0100

    RepositoryAuthentication: constructor, javadoc

--- src/main/java/org/minimalj/security/RepositoryAuthentication.java
+++ src/main/java/org/minimalj/security/RepositoryAuthentication.java
@@ -2,21 +2,28 @@ package org.minimalj.security;
 
 import java.util.List;
 
+import org.minimalj.backend.Backend;
 import org.minimalj.repository.Repository;
 import org.minimalj.repository.criteria.By;
 import org.minimalj.security.model.User;
 
+/**
+ * This Authentication holds user, passwords and roles in the applications
+ * repository. You have to add the User - class to your Application.getEntityClasses
+ * to let the Repository handle the needed classes.
+ *
+ */
 public class RepositoryAuthentication extends UserPasswordAuthentication {
 	private static final long serialVersionUID = 1L;
 
 	private final transient Repository authenticationRepository;
 	
-	public RepositoryAuthentication(Repository authenticationRepository) {
-		this.authenticationRepository = authenticationRepository;
+	public RepositoryAuthentication() {
+		this(Backend.getInstance().getRepository());
 	}
 	
-	protected RepositoryAuthentication() {
-		this.authenticationRepository = null;
+	protected RepositoryAuthentication(Repository authenticationRepository) {
+		this.authenticationRepository = authenticationRepository;
 	}
 	
 	protected User retrieveUser(String userName) {
