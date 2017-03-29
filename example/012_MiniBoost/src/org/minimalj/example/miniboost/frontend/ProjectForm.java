1323ecb754a665cabc1e9f776c809c1192c97dfb
bruno <bruno.eberhard@pop.ch>
2017-03-29 17:53:38 +0200

    ProjectForm: fixed country, chief field

--- example/012_MiniBoost/src/org/minimalj/example/miniboost/frontend/ProjectForm.java
+++ example/012_MiniBoost/src/org/minimalj/example/miniboost/frontend/ProjectForm.java
@@ -2,11 +2,15 @@ package org.minimalj.example.miniboost.frontend;
 
 import static org.minimalj.example.miniboost.model.Project.$;
 
+import java.util.Locale;
+
 import org.minimalj.example.miniboost.model.Country;
 import org.minimalj.example.miniboost.model.Customer;
+import org.minimalj.example.miniboost.model.Employee;
 import org.minimalj.example.miniboost.model.Project;
 import org.minimalj.frontend.form.Form;
 import org.minimalj.frontend.form.element.ReferenceFormElement;
+import org.minimalj.util.LocaleContext;
 
 public class ProjectForm extends Form<Project> {
 
@@ -18,13 +22,14 @@ public class ProjectForm extends Form<Project> {
 		line(new ReferenceFormElement<Customer>($.customer, new Object[]{ Customer.$.matchcode, Customer.$.name1, Customer.$.address.country, Customer.$.address.city }));
 		line($.description);
 		line($.address.street, $.address.zip);
-		line($.address.city, new ReferenceFormElement<>($.address.country, new Object[]{ Country.$.getName(), Country.$.id}) );
+		boolean german = LocaleContext.getCurrent().getLanguage().equals(new Locale("de").getLanguage());
+		line($.address.city, new ReferenceFormElement<>($.address.country, new Object[]{ german ? Country.$.nameDe : Country.$.nameEn, Country.$.id}) );
 		
 		text("Projektdaten");
 		line($.startDate, $.endDate);
 		line($.closeDate);
 
-		line($.crewChief);
+		line(new ReferenceFormElement<>($.crewChief, Employee.$.matchcode, Employee.$.firstname, Employee.$.lastname));
 		line($.amount, $.cost); // turnover and costs
 	}
 
