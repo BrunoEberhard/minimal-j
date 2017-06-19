5d3a76133448bb3dd89abc3d3c6e857802f97e84
bruno <bruno.eberhard@pop.ch>
2017-06-19 17:26:46 +0200

    backup for task example deployment on heroku

---
+++ example/heroku/src/main/java/org/minimalj/example/demo/ExamplesApplication.java
@@ -0,0 +1,33 @@
+package org.minimalj.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.minimalj.application.Application;
+import org.minimalj.application.ThreadLocalApplication;
+import org.minimalj.example.empty.EmptyApplication;
+import org.minimalj.example.helloworld.HelloWorldApplication;
+import org.minimalj.example.helloworld2.GreetingApplication;
+import org.minimalj.example.notes.NotesApplication;
+
+public class ExamplesApplication extends ThreadLocalApplication {
+
+	private final Map<String, Application> applications;
+	
+	public ExamplesApplication() {
+		applications = new HashMap<>();
+		applications.put("empty", new EmptyApplication());
+		applications.put("notes", new NotesApplication());
+		applications.put("helloWorld", new HelloWorldApplication());
+		applications.put("greeting", new GreetingApplication());
+//		applications.put("numbers", new NumbersApplication());
+//		applications.put("library", new MjExampleApplication());
+//		applications.put("petClinic", new PetClinicApplication());
+//		applications.put("minimalClinic", new MinimalClinicApplication());
+	}
+	
+	public void setCurrentApplication(String applicationName) {
+		setCurrentApplication(applications.get(applicationName));
+		
+	}
+}
