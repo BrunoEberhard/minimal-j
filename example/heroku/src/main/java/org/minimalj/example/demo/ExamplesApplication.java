6cddbd05df0c308dfd3d2bc2f3219b40f0df3be0
bruno <bruno.eberhard@pop.ch>
2017-07-06 10:35:04 +0200

    Activate more examples

--- example/heroku/src/main/java/org/minimalj/example/demo/ExamplesApplication.java
+++ example/heroku/src/main/java/org/minimalj/example/demo/ExamplesApplication.java
@@ -8,7 +8,10 @@ import org.minimalj.application.ThreadLocalApplication;
 import org.minimalj.example.empty.EmptyApplication;
 import org.minimalj.example.helloworld.HelloWorldApplication;
 import org.minimalj.example.helloworld2.GreetingApplication;
+import org.minimalj.example.library.MjExampleApplication;
+import org.minimalj.example.minimalclinic.MinimalClinicApplication;
 import org.minimalj.example.notes.NotesApplication;
+import org.minimalj.example.numbers.NumbersApplication;
 
 public class ExamplesApplication extends ThreadLocalApplication {
 
@@ -20,10 +23,10 @@ public class ExamplesApplication extends ThreadLocalApplication {
 		applications.put("notes", new NotesApplication());
 		applications.put("helloWorld", new HelloWorldApplication());
 		applications.put("greeting", new GreetingApplication());
-//		applications.put("numbers", new NumbersApplication());
-//		applications.put("library", new MjExampleApplication());
+		applications.put("numbers", new NumbersApplication());
+		applications.put("library", new MjExampleApplication());
 //		applications.put("petClinic", new PetClinicApplication());
-//		applications.put("minimalClinic", new MinimalClinicApplication());
+		applications.put("minimalClinic", new MinimalClinicApplication());
 	}
 	
 	public void setCurrentApplication(String applicationName) {
