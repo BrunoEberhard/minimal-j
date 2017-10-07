181db42e5c4e05d35e2beeffdbf5a66b3b34a065
Bruno Eberhard <eberhard@mptechnology.ch>
2017-10-07 11:43:08 +0200

    MjEntity: historized as internal field added

--- ext/metamodel/src/main/java/org/minimalj/metamodel/model/MjEntity.java
+++ ext/metamodel/src/main/java/org/minimalj/metamodel/model/MjEntity.java
@@ -44,7 +44,7 @@ public class MjEntity {
 		
 		Field[] fields = clazz.getFields();
 		for (Field field : fields) {
-			if (FieldUtils.isPublic(field) && !FieldUtils.isStatic(field) && !FieldUtils.isTransient(field) && !StringUtils.equals(field.getName(), "id", "version")) {
+			if (FieldUtils.isPublic(field) && !FieldUtils.isStatic(field) && !FieldUtils.isTransient(field) && !StringUtils.equals(field.getName(), "id", "version", "historized")) {
 				properties.add(new MjProperty(model, field));
 			}
 		}
