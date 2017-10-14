6bc3c18ef57312c50b69d59e0a3292adb76749c6
Bruno Eberhard <eberhard@mptechnology.ch>
2017-10-14 08:05:13 +0200

    Rest: transfer commit

--- ext/rest/src/main/java/org/minimalj/rest/RestHTTPD.java
+++ ext/rest/src/main/java/org/minimalj/rest/RestHTTPD.java
@@ -1,10 +1,13 @@
 package org.minimalj.rest;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
@@ -13,11 +16,14 @@ import java.util.Map;
 import org.minimalj.application.Application;
 import org.minimalj.application.Configuration;
 import org.minimalj.backend.Backend;
+import org.minimalj.metamodel.model.MjEntity;
+import org.minimalj.metamodel.model.MjModel;
 import org.minimalj.repository.query.By;
 import org.minimalj.repository.query.Query;
 import org.minimalj.repository.query.Query.QueryLimitable;
 import org.minimalj.rest.openapi.OpenAPIFactory;
 import org.minimalj.transaction.Transaction;
+import org.minimalj.util.IdUtils;
 import org.minimalj.util.StringUtils;
 import org.minimalj.util.resources.Resources;
 
@@ -26,7 +32,7 @@ import fi.iki.elonen.NanoHTTPD.Response.Status;
 
 public class RestHTTPD extends NanoHTTPD {
 
-	private Map<String, Class> classByName;
+	private final Map<String, Class> classByName;
 	
 	public RestHTTPD( int port, boolean secure) {
 		super(port);
@@ -40,25 +46,17 @@ public class RestHTTPD extends NanoHTTPD {
 				e.printStackTrace();
 			}
 		}
+		classByName = initClassMap();
 	}
 	
-	protected Map<String, Class> getClassByName() {
-		if (classByName == null) {
-			classByName = new HashMap<>();
-			for (Class entitiyClass : Application.getInstance().getEntityClasses()) {
-				String name = entitiyClass.getSimpleName();
-				if (classByName.containsKey(name)) {
-					throw new IllegalArgumentException("Application contains two entity classes with same SimpleName " + name);
-				}
-				classByName.put(name, entitiyClass);
-			}
+	protected Map<String, Class> initClassMap() {
+		Map<String, Class> classByName = new HashMap<>();
+		MjModel model = new MjModel(Application.getInstance().getEntityClasses());
+		for (MjEntity entity : model.entities) {
+			classByName.put(entity.getClazz().getSimpleName(), entity.getClazz());
 		}
+		
 		return classByName;
-	}
-	
-	
-	protected Class<?> getClass(String simpleName) {
-		return getClassByName().get(simpleName);
 	}
 	
 	@Override
@@ -76,97 +74,157 @@ public class RestHTTPD extends NanoHTTPD {
 		} catch (URISyntaxException e) {
 			return newFixedLengthResponse(Status.BAD_REQUEST, "text/html", e.getMessage());
 		}
-
+		
+		Class<?> clazz = null;
+		if (pathElements.length > 0 && !Character.isLowerCase(pathElements[0].charAt(0))) {
+			clazz = classByName.get(pathElements[0]);
+			if (clazz == null) {
+				return newFixedLengthResponse(Status.NOT_FOUND, "text/html", "Class not available");
+			}
+		}
+		
 		if (method == Method.GET) {
-				if (pathElements.length == 0) {
-					return newFixedLengthResponse(Status.BAD_REQUEST, "text/html", "Please specify class");
-				}
-				if (StringUtils.equals("swagger-ui", pathElements[0])) {
-					if (pathElements.length == 1) {
-						return newChunkedResponse(Status.OK, "text/html", getClass().getResourceAsStream(uriString + "/index.html"));
-					} else if (StringUtils.equals("swagger.json", pathElements[1])) {
-						return newFixedLengthResponse(Status.OK, "text/json", OpenAPIFactory.create(Application.getInstance()));
-					} else {
-						int pos = uriString.lastIndexOf('.');
-						String mimeType = Resources.getMimeType(uriString.substring(pos + 1));
-						return newChunkedResponse(Status.OK, mimeType, getClass().getResourceAsStream(uriString));
-					}
-				}
-				Class<?> clazz = getClass(pathElements[0]);
-				if (clazz == null) {
-					return newFixedLengthResponse(Status.NOT_FOUND, "text/html", "Class not available");
-				}
+			if (pathElements.length == 0) {
+				return newFixedLengthResponse(Status.BAD_REQUEST, "text/html", "Please specify class");
+			}
+			if (StringUtils.equals("swagger-ui", pathElements[0])) {
 				if (pathElements.length == 1) {
-					// GET entity (get all or pages of size x)
-					Query query = By.all();
-					String sizeParameter = parameters.get("size");
-					if (!StringUtils.isBlank(sizeParameter)) {
-						int page = 0;
-						String pageParameter = parameters.get("page");
-						if (!StringUtils.isBlank(pageParameter)) {
-							try {
-								page = Integer.valueOf(pageParameter);
-							} catch (NumberFormatException e) {
-								return newFixedLengthResponse(Status.BAD_REQUEST, "text/json", "page parameter invalid: " + pageParameter);
-							}
-						}
+					return newChunkedResponse(Status.OK, "text/html",
+							getClass().getResourceAsStream(uriString + "/index.html"));
+				} else if (StringUtils.equals("swagger.json", pathElements[1])) {
+					return newFixedLengthResponse(Status.OK, "text/json",
+							new OpenAPIFactory().create(Application.getInstance()));
+				} else {
+					int pos = uriString.lastIndexOf('.');
+					String mimeType = Resources.getMimeType(uriString.substring(pos + 1));
+					return newChunkedResponse(Status.OK, mimeType, getClass().getResourceAsStream(uriString));
+				}
+			}
+			if (pathElements.length == 1) {
+				// GET entity (get all or pages of size x)
+				Query query = By.all();
+				String sizeParameter = parameters.get("size");
+				if (!StringUtils.isBlank(sizeParameter)) {
+					int page = 0;
+					String pageParameter = parameters.get("page");
+					if (!StringUtils.isBlank(pageParameter)) {
 						try {
-							int size = Integer.valueOf(sizeParameter);
-							query = ((QueryLimitable) query).limit(page != 0 ? page * size : null, size);
+							page = Integer.valueOf(pageParameter);
 						} catch (NumberFormatException e) {
-							return newFixedLengthResponse(Status.BAD_REQUEST, "text/json", "size parameter invalid: " + sizeParameter);
+							return newFixedLengthResponse(Status.BAD_REQUEST, "text/json",
+									"page parameter invalid: " + pageParameter);
 						}
 					}
-					List<?> object = Backend.find(clazz, query);
-					return newFixedLengthResponse(Status.OK, "text/json", new EntityJsonWriter().write(object));
+					try {
+						int size = Integer.valueOf(sizeParameter);
+						query = ((QueryLimitable) query).limit(page != 0 ? page * size : null, size);
+					} catch (NumberFormatException e) {
+						return newFixedLengthResponse(Status.BAD_REQUEST, "text/json",
+								"size parameter invalid: " + sizeParameter);
+					}
 				}
-				if (pathElements.length == 2) {
-					// GET entity/id (get one)
-					String id = pathElements[1];
-					Object object = Backend.read(clazz, id);
-					return newFixedLengthResponse(Status.OK, "text/json", new EntityJsonWriter().write(object));
-				}
+				List<?> object = Backend.find(clazz, query);
+				return newFixedLengthResponse(Status.OK, "text/json", new EntityJsonWriter().write(object));
+			}
+			if (pathElements.length == 2) {
+				// GET entity/id (get one)
+				String id = pathElements[1];
+				Object object = Backend.read(clazz, id);
+				return newFixedLengthResponse(Status.OK, "text/json", new EntityJsonWriter().write(object));
+			}
 		} else if (method == Method.POST) {
-			if (StringUtils.equals("java-transaction", pathElements[0])) {
-				if (pathElements.length == 1 && pathElements[0].equals("java-transaction")) {
-					Object input;
-					String inputString = files.get("PostData");
+			if (clazz != null) {
+				if (pathElements.length >= 2) {
+					String id = pathElements[1];
+					String inputString = files.get("content");
 					if (inputString == null) {
 						return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "No Input");
 					}
-					try {
-						byte[] inputByteArray = Base64.getDecoder().decode(inputString);
-						try (ByteArrayInputStream bis = new ByteArrayInputStream(inputByteArray)) {
-							try (ObjectInputStream ois = new ObjectInputStream(bis)) {
-								input = ois.readObject();
-							} catch (Exception x) {
-								return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "Failed to read input: " + x.getMessage());
-							}
-						} catch (IOException e) {
-							return newFixedLengthResponse(Status.INTERNAL_ERROR, "text/plain", "IOException " + e.getMessage());
+					Object inputObject = new EntityJsonReader().read(inputString);
+					Backend.update(inputObject);
+ 				} else {
+ 					return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "Post excepts id in url");
+ 				}
+			}
+			if (pathElements.length > 0) {
+				if (StringUtils.equals("java-transaction", pathElements[0])) {
+					if (pathElements.length == 1 && pathElements[0].equals("java-transaction")) {
+						Object input;
+						String inputString = files.get("content");
+						if (inputString == null) {
+							return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "No Input");
 						}
-					} catch (IllegalArgumentException x) {
-						return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "Input not in valid Base64 scheme");
+						try {
+							byte[] inputByteArray = Base64.getDecoder().decode(inputString);
+							try (ByteArrayInputStream bis = new ByteArrayInputStream(inputByteArray)) {
+								try (ObjectInputStream ois = new ObjectInputStream(bis)) {
+									input = ois.readObject();
+								} catch (Exception x) {
+									return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "Failed to read input: " + x.getMessage());
+								}
+							} catch (IOException e) {
+								return newFixedLengthResponse(Status.INTERNAL_ERROR, "text/plain", "IOException " + e.getMessage());
+							}
+						} catch (IllegalArgumentException x) {
+							return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "Input not in valid Base64 scheme");
+						}
+						if (input == null) {
+							return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "No input");
+						}
+						if (!(input instanceof Transaction)) {
+							return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "Input not a Transaction but a " + input.getClass().getName());
+						}
+						
+						Object output = ((Transaction<?>) input).execute();
+						
+						// TODO
+	//					ByteArrayOutputStream bos = new ByteArrayOutputStream(outputByteArray);
+	//					
+	//					byte[] outputByteArray = new ObjectOutputStream(new ByteArrayOutputStream());
+	//
+	//					String outputString = Base64.getEncoder().encodeToString(outputByteArray);
+	//					return newFixedLengthResponse(Status.OK, "application/base64", outputString);
+						
 					}
-					if (input == null) {
-						return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "No input");
-					}
-					if (!(input instanceof Transaction)) {
-						return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "Input not a Transaction but a " + input.getClass().getName());
-					}
-					
-					Object output = ((Transaction<?>) input).execute();
-					
-					// TODO
-//					ByteArrayOutputStream bos = new ByteArrayOutputStream(outputByteArray);
-//					
-//					byte[] outputByteArray = new ObjectOutputStream(new ByteArrayOutputStream());
-//
-//					String outputString = Base64.getEncoder().encodeToString(outputByteArray);
-//					return newFixedLengthResponse(Status.OK, "application/base64", outputString);
+				} else {
 					
 				}
 			}
+		} else if (method == Method.PUT) {
+			if (clazz != null) {
+				String inputFileName = files.get("content");
+				if (inputFileName == null) {
+					return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "No Input");
+				}
+				String input = "";
+				try {
+					List<String> inputLines = Files.readAllLines(new File(inputFileName).toPath());
+					for (String line : inputLines) {
+						input = input + line;
+					}
+					Object inputObject = new EntityJsonReader().read(clazz, input);
+					// IdUtils.setId(inputObject, null);
+					Backend.insert(inputObject);
+				} catch (IOException x) {
+					return newFixedLengthResponse(Status.BAD_REQUEST, "text/plain", "Could not read input");
+				}
+			}
+		} else if (method == Method.OPTIONS) {
+			
+//			TODO thank on StackOverflow
+//	        header('Access-Control-Allow-Origin: *');
+//	        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+//	        header('Access-Control-Allow-Headers: API-Key,accept, Content-Type');
+//	        header('Access-Control-Max-Age: 1728000');
+	        
+	        
+			Response response = newFixedLengthResponse(Status.OK, "text/plain", null);
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			response.addHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, PATCH, OPTIONS");
+			response.addHeader("Access-Control-Allow-Headers", "API-Key,accept, Content-Type");
+			response.addHeader("Access-Control-Max-Age", "1728000");
+			
+			return response;
 		} else {
 			return null;
 		}
--- ext/rest/src/main/java/org/minimalj/rest/openapi/OpenAPIFactory.java
+++ ext/rest/src/main/java/org/minimalj/rest/openapi/OpenAPIFactory.java
@@ -1,14 +1,18 @@
 package org.minimalj.rest.openapi;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.minimalj.application.Application;
 import org.minimalj.metamodel.model.MjEntity;
 import org.minimalj.metamodel.model.MjModel;
 import org.minimalj.metamodel.model.MjProperty;
 import org.minimalj.metamodel.model.MjProperty.MjPropertyType;
+import org.minimalj.model.EnumUtils;
 import org.minimalj.rest.EntityJsonWriter;
 import org.minimalj.rest.openapi.model.OpenAPI;
 import org.minimalj.rest.openapi.model.OpenAPI.Content;
@@ -16,44 +20,91 @@ import org.minimalj.rest.openapi.model.OpenAPI.In;
 import org.minimalj.rest.openapi.model.OpenAPI.Operation;
 import org.minimalj.rest.openapi.model.OpenAPI.Parameter;
 import org.minimalj.rest.openapi.model.OpenAPI.Property;
+import org.minimalj.rest.openapi.model.OpenAPI.RequestBody;
 import org.minimalj.rest.openapi.model.OpenAPI.Response;
 import org.minimalj.rest.openapi.model.OpenAPI.Schema;
+import org.minimalj.rest.openapi.model.OpenAPI.Server;
 import org.minimalj.rest.openapi.model.OpenAPI.Type;
 import org.minimalj.util.FieldUtils;
 import org.minimalj.util.IdUtils;
-import org.minimalj.util.StringUtils;
 
 public class OpenAPIFactory {
 	
-	public static String create(Application application) {
+	public static enum API {
+		Swagger, OpenAPI3;
+	}
+	
+	private final API api;
+	private final String SCHEMAS;
+	
+	public OpenAPIFactory() {
+		this(API.OpenAPI3);
+	}
+	
+	public OpenAPIFactory(API api) {
+		this.api = api;
+		if (api == API.Swagger) {
+			SCHEMAS = "#/definitions/";
+		} else {
+			SCHEMAS = "#/components/schemas/";
+		}
+	}
+
+	public String create(Application application) {
 		OpenAPI api = new OpenAPI();
-		api.openapi = "3.0.0";
+		if (this.api == API.OpenAPI3) {
+			api.openapi = "3.0.0";
+			api.components = new OpenAPI.Components();
+		} else {
+			api.swagger = "2.0";
+		}
+		
 		api.info = new OpenAPI.Info();
 		api.info.version = "1.0.0";
 		api.info.title = application.getName();
+		
+		Server server = new Server();
+		server.url = "http://localhost:8090/";
+		api.servers.add(server);
 
-		api.components = new OpenAPI.Components();
 		MjModel model = new MjModel(application.getEntityClasses());
 		for (MjEntity entity : model.entities) {
 			String entityName = entity.getClazz().getSimpleName();
-			String entityNameLower = StringUtils.lowerFirstChar(entityName);
 
 			if (IdUtils.hasId(entity.getClazz())) {
 				Map<String, Operation> operations = new HashMap<>();
-				
-				Operation operation = operationGetById(entityName);
+				Operation operation = operationGetById(entity);
 				operations.put("get", operation);
-				api.paths.put("/" + entityNameLower + "s/{id}", operations);
+				api.paths.put("/" + entityName + "/{id}", operations);
 				
 				operations = new HashMap<>();
 				
-				operation = operationGetAll(entityName);
+				operation = operationGetAll(entity);
 				operations.put("get", operation);
-				api.paths.put(entityNameLower + "s/{id}", operations);
+				
+				operation = operationPut(entity);
+				operations.put("put", operation);
+				
+				api.paths.put("/" + entityName, operations);
 			}
 			
-			Schema schema = schema(entity);
-			api.components.schemas.put(entityName, schema);
+			Schema schema;
+			if (Enum.class.isAssignableFrom(entity.getClazz())) {
+				if (this.api == API.OpenAPI3) {
+					schema = eNum(entity);
+				} else {
+					// v2 has no reusable enum
+					continue;
+				}
+			} else {
+				schema = schema(entity);
+			}
+
+			if (this.api == API.OpenAPI3) {
+				api.components.schemas.put(entityName, schema);
+			} else {
+				api.definitions.put(entityName, schema);
+			}	
 		}
 		
 		EntityJsonWriter writer = new EntityJsonWriter();
@@ -61,7 +112,9 @@ public class OpenAPIFactory {
 	}
 
 
-	private static Operation operationGetById(String entityName) {
+	private Operation operationGetById(MjEntity entity) {
+		String entityName = entity.getClazz().getSimpleName();
+		
 		Operation operation = new Operation();
 		operation.summary = "Gets a " + entityName + " by id";
 		
@@ -71,60 +124,114 @@ public class OpenAPIFactory {
 		parameter.in = In.path;
 		parameter.description = entityName + " id";
 		
-		Schema schema = new Schema();
-		schema.type = Type.STRING;
-		parameter.schema = schema;
+		if (api == API.OpenAPI3) {
+			Schema schema = new Schema();
+			schema.type = Type.STRING;
+			parameter.schema = schema;
+		} else {
+			parameter.type = "string";
+		}
 		
 		operation.parameters.add(parameter);
 		
 		Response response = new Response();
 		response.description = "Successful operation";
 		
-		schema = new Schema();
-		schema.$ref = "#/components/schemas/" + entityName;
+		Schema schema = new Schema();
+		schema.$ref = SCHEMAS + entityName;
 
-		Content content = new Content();
-		content.schema = schema;
+		if (api == API.OpenAPI3) {
+			Content content = new Content();
+			content.schema = schema;
 
-		response.content.put("application/json", content);
+			response.content.put("application/json", content);
+		} else {
+			response.schema = schema;
+		}
 		
 		operation.responses.put("200", response);
 		return operation;
 	}
 
 
-	private static Operation operationGetAll(String entityName) {
+	private Operation operationGetAll(MjEntity entity) {
+		String entityName = entity.getClazz().getSimpleName();
+
 		Operation operation = new Operation();
 		operation.summary = "Gets all " + entityName;
+
+		Response response = new Response();
+		response.description = "Successful operation";
+
+		if (api == API.OpenAPI3) {
+			Schema schema = new Schema();
+			schema.type = OpenAPI.Type.ARRAY;
+			schema.$ref = SCHEMAS + entityName;
+
+			Content content = new Content();
+			content.schema = schema;
+
+			response.content.put("application/json", content);
+		} else {
+			Schema schema = new Schema();
+			schema.type = OpenAPI.Type.ARRAY;
+			Schema itemSchema = new Schema();
+			itemSchema.$ref = SCHEMAS + entityName;
+			schema.items = itemSchema;
+
+			response.schema = schema;
+		}
+
+		operation.responses.put("200", response);
+		return operation;
+	}
+	
+	private Operation operationPut(MjEntity entity) {
+		String entityName = entity.getClazz().getSimpleName();
+		
+		Operation operation = new Operation();
+		operation.summary = "Add a new " + entityName;
+			
+		Schema schema = new Schema();
+		schema.$ref = SCHEMAS + entityName;
+		
+		if (api == API.OpenAPI3) {
+			Content content = new Content();
+			content.schema = schema;
+			
+			RequestBody requestBody = new RequestBody();
+			requestBody.content.put("application/json", content);
+
+			operation.requestBody = requestBody;
+		} else {
+			// TODO response.schema = schema;
+		}
 		
 		Response response = new Response();
 		response.description = "Successful operation";
-		
-		Schema schema = new Schema();
-		schema.type = OpenAPI.Type.ARRAY;
-		schema.$ref = "#/components/schemas/" + entityName;
-
-		Content content = new Content();
-		content.schema = schema;
-
-		response.content.put("application/json", content);
 		
 		operation.responses.put("200", response);
 		return operation;
 	}
 	
-	private static Schema schema(MjEntity entity) {
+	private Schema schema(MjEntity entity) {
 		Schema schema = new Schema();
 
 		Property property = new Property();
-		property.nullable = true;
+		if (api == API.OpenAPI3 ) {
+			property.nullable = true;
+		}
+		schema.required.add("id");
 		property.type = OpenAPI.Type.STRING;
 		schema.properties.put("id", property);
 		
 		boolean hasVersion = FieldUtils.hasValidVersionfield(entity.getClazz());
 		if (hasVersion) {
 			property = new Property();
-			property.nullable = true;
+			if (api == API.OpenAPI3 ) {
+				property.nullable = true;
+			}
+			schema.required.add("version");
 			property.type = OpenAPI.Type.INTEGER;
 			schema.properties.put("version", property);
 		}
@@ -132,16 +239,48 @@ public class OpenAPIFactory {
 		boolean historized = FieldUtils.hasValidHistorizedField(entity.getClazz());
 		if (historized) {
 			property = new Property();
-			property.nullable = true;
+			if (api == API.OpenAPI3) {
+				property.nullable = true;
+			}
+			schema.required.add("historized");
 			property.type = OpenAPI.Type.BOOLEAN;
 			schema.properties.put("historized", property);
 		}
 		
 		for (MjProperty mjProperty : entity.properties) {
 			property = new Property();
-			property.nullable = mjProperty.notEmpty ? null : true; // nullable false is default, omit
+			if (api == API.OpenAPI3 ) {
+				property.nullable = mjProperty.notEmpty ? null : true; // nullable false is default, omit
+			}
+			if (mjProperty.notEmpty) {
+				schema.required.add(mjProperty.name);
+			}
 			property.type = type(mjProperty);
-			property.$ref = ref(mjProperty);
+			
+			if (api == API.OpenAPI3) {
+				property.$ref = ref(mjProperty);
+				if (mjProperty.propertyType == MjPropertyType.ENUM) {
+					property.type = null;
+					property.$ref = SCHEMAS + mjProperty.type.getClazz().getSimpleName();
+				}
+					
+			} else {
+				if (property.type == Type.ARRAY) {
+					property.items = schema(mjProperty.type);
+				} else if (mjProperty.propertyType == MjPropertyType.ENUM) {
+					// OpenApi3 has reusable enums
+					property.eNum = new ArrayList<>();
+					for (Object e : EnumUtils.valueList((Class<? extends Enum>) mjProperty.type.getClazz())) {
+						property.eNum.add(e.toString());
+					}
+				} else {
+					property.$ref = ref(mjProperty);
+				}
+				if (property.$ref != null) {
+					property.type = null;
+				}
+			}
+			
 			property.format = format(mjProperty);
 			property.items = items(mjProperty);
 			schema.properties.put(mjProperty.name, property);
@@ -149,7 +288,18 @@ public class OpenAPIFactory {
 		return schema;
 	}
 	
-	private static OpenAPI.Type type(MjProperty property) {
+	// V3
+	private Schema eNum(MjEntity entity) {
+		Schema schema = new Schema();
+
+		schema.type = OpenAPI.Type.STRING;
+		List value = EnumUtils.valueList((Class<? extends Enum>) entity.getClazz());
+		schema.eNum = (List<String>) value.stream().map(e -> e.toString()).collect(Collectors.toList());
+		
+		return schema;
+	}
+	
+	private OpenAPI.Type type(MjProperty property) {
 		switch (property.propertyType) {
 		case String:
 		case LocalDate:
@@ -173,15 +323,15 @@ public class OpenAPIFactory {
 		}
 	}
 	
-	private static String ref(MjProperty property) {
+	private String ref(MjProperty property) {
 		if (property.propertyType == MjPropertyType.INLINE ||property.propertyType == MjPropertyType.DEPENDABLE) {
-			return "#/components/schemas/" + property.type.getClazz().getSimpleName();
+			return SCHEMAS + property.type.getClazz().getSimpleName();
 		} else {
 			return null;
 		}
 	}
 	
-	private static String format(MjProperty property) {
+	private String format(MjProperty property) {
 		switch(property.propertyType) {
 		case String : return null;
 		case Integer: return "int32";
@@ -193,7 +343,7 @@ public class OpenAPIFactory {
 		}
 	}
 
-	private static Schema items(MjProperty property) {
+	private Schema items(MjProperty property) {
 		if (property.propertyType == MjPropertyType.LIST && IdUtils.hasId(property.type.getClazz())) {
 			Schema schema = new Schema();
 			schema.type = OpenAPI.Type.STRING;
@@ -201,7 +351,7 @@ public class OpenAPIFactory {
 		}
 		if (property.propertyType == MjPropertyType.LIST || property.propertyType == MjPropertyType.ENUM_SET) {
 			Schema schema = new Schema();
-			schema.$ref = "#/components/schemas/" + property.type.getClazz().getSimpleName();
+			schema.$ref = SCHEMAS + property.type.getClazz().getSimpleName();
 			return schema;
 		} else {
 			return null;
