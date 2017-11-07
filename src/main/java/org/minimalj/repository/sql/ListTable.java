7f3768c5425c9af73f9028bd3ad7cafb41488b41
Bruno Eberhard <eberhard@mptechnology.ch>
2017-11-07 06:27:41 +0100

    ListTable: removed dead inner interface

--- src/main/java/org/minimalj/repository/sql/ListTable.java
+++ src/main/java/org/minimalj/repository/sql/ListTable.java
@@ -10,26 +10,4 @@ public interface ListTable<PARENT, ELEMENT> {
 
 	public void replaceList(PARENT parent, List<ELEMENT> objects);
 	
-	//
-	
-	public interface HistorizedListTable<PARENT, ELEMENT> extends ListTable<PARENT, ELEMENT> {
-		
-		public List<ELEMENT> readAll(PARENT parent, Integer time);
-		
-		public void readVersions(Object parentId, List<Integer> result);
-
-		@Override
-		default void addList(PARENT parent, List<ELEMENT> objects) {
-			addList(parent, objects, 0);
-		}
-
-		@Override
-		default void replaceList(PARENT parent, List<ELEMENT> objects) {
-			throw new IllegalArgumentException();
-		}
-
-		public void addList(PARENT parent, List<ELEMENT> objects, Integer version);
-
-		public void replaceList(PARENT parent, List<ELEMENT> objects, int version);
-	}
 }
