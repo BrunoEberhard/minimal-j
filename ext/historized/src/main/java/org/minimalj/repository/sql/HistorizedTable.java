d1e9dfc2b879eab98aa9a9aefc41be81669976d6
Bruno Eberhard <eberhard@mptechnology.ch>
2017-11-07 07:17:54 +0100

    HistorizedTable: don't create constraints

--- ext/historized/src/main/java/org/minimalj/repository/sql/HistorizedTable.java
+++ ext/historized/src/main/java/org/minimalj/repository/sql/HistorizedTable.java
@@ -179,6 +179,11 @@ public class HistorizedTable<T> extends Table<T> {
 		}
 	}		
 	
+	@Override
+	public void createConstraints(SqlDialect dialect) {
+		// not possible for historized tables
+	}
+	
 	// Statements
 
 	@Override
