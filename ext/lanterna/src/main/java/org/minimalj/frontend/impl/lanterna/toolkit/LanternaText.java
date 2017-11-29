996085376d912c59971fa4bafa02a406434542b2
Bruno Eberhard <bruno.eberhard@pop.ch>
2017-11-29 19:53:04 +0100

    Security: added warning on HtmlPage

--- ext/lanterna/src/main/java/org/minimalj/frontend/impl/lanterna/toolkit/LanternaText.java
+++ ext/lanterna/src/main/java/org/minimalj/frontend/impl/lanterna/toolkit/LanternaText.java
@@ -3,13 +3,14 @@ package org.minimalj.frontend.impl.lanterna.toolkit;
 import org.minimalj.frontend.Frontend.IComponent;
 import org.minimalj.model.Rendering;
 import org.minimalj.model.Rendering.RenderType;
+import org.minimalj.util.StringUtils;
 
 import com.googlecode.lanterna.gui2.Label;
 
 public class LanternaText extends Label implements IComponent {
 
 	public LanternaText(String text) {
-		super(text);
+		super(StringUtils.stripHtml(text));
 	}
 
 	public LanternaText(Rendering rendering) {
@@ -18,7 +19,7 @@ public class LanternaText extends Label implements IComponent {
 
 	@Override
 	public void setText(String text) {
-		super.setText(text != null ? text : "");
+		super.setText(text != null ? StringUtils.stripHtml(text) : "");
 	}
-
+	
 }
--- src/main/java/org/minimalj/frontend/impl/json/JsonText.java
+++ src/main/java/org/minimalj/frontend/impl/json/JsonText.java
@@ -11,6 +11,7 @@ public class JsonText extends JsonComponent {
 		if (object != null) {
 			String string = object.toString();
 			if (StringUtils.isHtml(string)) {
+				string = StringUtils.sanitizeHtml(string);
 				put("htmlValue", string);
 			} else {
 				put(JsonInputComponent.VALUE, string);
@@ -22,10 +23,9 @@ public class JsonText extends JsonComponent {
 		super("Text");
 		if (rendering != null) {
 			RenderType renderType = rendering.getPreferredRenderType(RenderType.HMTL, RenderType.PLAIN_TEXT);
-			String s = rendering.render(renderType);
-			// Security: should the rendered string be checked for attacks?
-			// the value is later inserted in the html page as innerHtml
-			put(renderType == RenderType.HMTL ? "htmlValue" : JsonInputComponent.VALUE, s);
+			String string = rendering.render(renderType);
+			string = StringUtils.sanitizeHtml(string);
+			put(renderType == RenderType.HMTL ? "htmlValue" : JsonInputComponent.VALUE, string);
 		}
 	}
 }
