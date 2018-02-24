280fed39663f206b19a9e69ab6f327909fc8390b
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-02-24 11:29:55 +0100

    Server and sender class for cheerpj

--- src/main/java/org/minimalj/frontend/impl/cheerpj/Cheerpj.java
+++ src/main/java/org/minimalj/frontend/impl/cheerpj/Cheerpj.java
@@ -3,7 +3,11 @@ package org.minimalj.frontend.impl.cheerpj;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.minimalj.application.Application;
+import org.minimalj.frontend.Frontend;
+import org.minimalj.frontend.impl.json.JsonFrontend;
 import org.minimalj.frontend.impl.json.JsonPageManager;
+import org.minimalj.model.test.ModelTest;
 
 public class Cheerpj {
 
@@ -22,4 +26,19 @@ public class Cheerpj {
 		}
 	}
 
+	public static void start() {
+		ModelTest.exitIfProblems();
+		Frontend.setInstance(new JsonFrontend());
+	}
+	
+	public static void start(Application application) {
+		Application.setInstance(application);
+		start();
+	}
+	
+	public static void main(String... args) {
+		Application.initApplication(args);
+		start();
+	}
+	
 }
