be8fa8baed79d161b8c964697175ca57d4765e83
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-04-04 18:08:04 +0200

    Ubersetzung: adapt to Minimal-J changes

--- ext/ubersetzung/src/main/java/org/minimalj/ubersetzung/frontend/UbersetzungTablePage.java
+++ ext/ubersetzung/src/main/java/org/minimalj/ubersetzung/frontend/UbersetzungTablePage.java
@@ -1,21 +1,18 @@
 package org.minimalj.ubersetzung.frontend;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.minimalj.backend.Backend;
-import org.minimalj.frontend.action.Action;
-import org.minimalj.frontend.editor.Editor.NewObjectEditor;
 import org.minimalj.frontend.form.Form;
-import org.minimalj.frontend.page.TablePage.TablePageWithDetail;
+import org.minimalj.frontend.page.TableEditorPage;
 import org.minimalj.repository.query.By;
 import org.minimalj.ubersetzung.model.Ubersetzung;
 import org.minimalj.ubersetzung.model.UbersetzungView;
 
-public class UbersetzungTablePage extends TablePageWithDetail<UbersetzungView, UbersetzungEntryTablePage> {
+public class UbersetzungTablePage extends TableEditorPage<UbersetzungView, Ubersetzung> {
 
 	public UbersetzungTablePage() {
-		super(new Object[] {UbersetzungView.$.lang, UbersetzungView.$.country});
+		super(new Object[] { UbersetzungView.$.lang, UbersetzungView.$.country });
 	}
 
 	@Override
@@ -24,41 +21,17 @@ public class UbersetzungTablePage extends TablePageWithDetail<UbersetzungView, U
 	}
 
 	@Override
-	protected UbersetzungEntryTablePage createDetailPage(UbersetzungView viewObject) {
+	protected UbersetzungEntryTablePage getDetailPage(UbersetzungView viewObject) {
 		Ubersetzung ubersetzung = Backend.read(Ubersetzung.class, viewObject.id);
 		return new UbersetzungEntryTablePage(ubersetzung);
 	}
 
 	@Override
-	protected UbersetzungEntryTablePage updateDetailPage(UbersetzungEntryTablePage page, UbersetzungView viewObject) {
-		Ubersetzung ubersetzung = Backend.read(Ubersetzung.class, viewObject.id);
-		page.setUbersetzung(ubersetzung);
-		return page;
-	}
-	
-	@Override
-	public List<Action> getActions() {
-		List<Action> actions = new ArrayList<>();
-		actions.add(new NewUbersetzungAction());
-		return actions;
-	}
-
-	public class NewUbersetzungAction extends NewObjectEditor<Ubersetzung> {
-
-		@Override
-		protected Form<Ubersetzung> createForm() {
-			Form<Ubersetzung> form = new Form<>();
-			form.line(Ubersetzung.$.lang);
-			form.line(Ubersetzung.$.country);
-			return form;
-		}
-
-		@Override
-		protected Ubersetzung save(Ubersetzung object) {
-			object = Backend.save(object);
-			refresh();
-			return object;
-		}
+	protected Form<Ubersetzung> createForm(boolean editable, boolean newObject) {
+		Form<Ubersetzung> form = new Form<>(editable);
+		form.line(Ubersetzung.$.lang);
+		form.line(Ubersetzung.$.country);
+		return form;
 	}
 
 }
