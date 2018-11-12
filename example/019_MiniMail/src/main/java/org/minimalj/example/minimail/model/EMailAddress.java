0464e27bd745166c9e70495fcd634c465a7ddbc4
Marco Musolf <marcomusolf@hotmail.com>
2018-11-12 16:39:14 +0100

    added mail address validation

--- example/019_MiniMail/src/main/java/org/minimalj/example/minimail/model/EMailAddress.java
+++ example/019_MiniMail/src/main/java/org/minimalj/example/minimail/model/EMailAddress.java
@@ -1,11 +1,41 @@
 package org.minimalj.example.minimail.model;
 
-import org.minimalj.model.Keys;
-import org.minimalj.model.annotation.Size;
+import java.util.List;
 
-public class EMailAddress {
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import org.minimalj.model.Keys;
+import org.minimalj.model.annotation.NotEmpty;
+import org.minimalj.model.annotation.Size;
+import org.minimalj.model.validation.Validation;
+import org.minimalj.model.validation.ValidationMessage;
+import org.minimalj.util.resources.Resources;
+
+public class EMailAddress implements Validation {
 	public static final EMailAddress $ = Keys.of(EMailAddress.class);
 
 	@Size(2000)
+	@NotEmpty
 	public String address;
+	
+	@Override
+	public List<ValidationMessage> validate() {
+		if (address != null && !isValidEmailAddress(address)) {
+			return Validation.message($.address, Resources.getString("Mail.address.validation.error"));
+		}
+		return null;
+	}
+
+	private static boolean isValidEmailAddress(String email) {
+		boolean result = true;
+		try {
+			InternetAddress emailAddr = new InternetAddress(email);
+			emailAddr.validate();
+		} catch (AddressException ex) {
+			result = false;
+		}
+		return result;
+	}
+
 }
--- example/019_MiniMail/src/main/java/org/minimalj/example/minimail/model/Mail.java
+++ example/019_MiniMail/src/main/java/org/minimalj/example/minimail/model/Mail.java
@@ -3,9 +3,10 @@ package org.minimalj.example.minimail.model;
 import java.time.LocalDate;
 
 import org.minimalj.model.Keys;
+import org.minimalj.model.annotation.NotEmpty;
 import org.minimalj.model.annotation.Size;
 
-public class Mail {
+public class Mail  {
 	public static final Mail $ = Keys.of(Mail.class);
 
 	public Object id;
@@ -14,10 +15,14 @@ public class Mail {
 	public final EMailAddress to = new EMailAddress();
 	
 	@Size(2000)
+	@NotEmpty
 	public String subject;
 	
 	public LocalDate date;
 	
 	@Size(20000)
+	@NotEmpty
 	public String text;
+	
+	
 }
