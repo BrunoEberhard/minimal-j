ff3f6ca7ac38dc55c5c7a9d848b78ea1faff7992
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-11-18 13:07:37 +0100

    ReferenceFormElement: newForm

--- src/main/java/org/minimalj/frontend/form/element/ReferenceFormElement.java
+++ src/main/java/org/minimalj/frontend/form/element/ReferenceFormElement.java
@@ -2,12 +2,15 @@ package org.minimalj.frontend.form.element;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.minimalj.backend.Backend;
 import org.minimalj.frontend.Frontend.Search;
 import org.minimalj.frontend.Frontend.TableActionListener;
 import org.minimalj.frontend.action.Action;
+import org.minimalj.frontend.editor.Editor.NewObjectEditor;
 import org.minimalj.frontend.editor.SearchDialog;
+import org.minimalj.frontend.form.Form;
 import org.minimalj.model.annotation.NotEmpty;
 import org.minimalj.repository.query.By;
 import org.minimalj.repository.sql.EmptyObjects;
@@ -17,6 +20,7 @@ import org.minimalj.repository.sql.EmptyObjects;
 public class ReferenceFormElement<T> extends AbstractLookupFormElement {
 	private final Class<T> fieldClazz;
 	private final Object[] searchColumns;
+	private Form<T> newForm;
 	private SearchDialog<T> dialog;
 
 	@SuppressWarnings("unchecked")
@@ -28,12 +32,17 @@ public class ReferenceFormElement<T> extends AbstractLookupFormElement {
 
 	@Override
 	protected void lookup() {
-		dialog = new SearchDialog<>(new ReferenceFieldSearch(), searchColumns, false, new SearchDialogActionListener(), createAdditionalActions());
+		List<Action> additionalActions = createAdditionalActions();
+		additionalActions = additionalActions.stream().filter(Action::isEnabled).collect(Collectors.toList());
+		dialog = new SearchDialog<>(new ReferenceFieldSearch(), searchColumns, false, new SearchDialogActionListener(), additionalActions);
 		dialog.show();
 	}
 
 	protected List<Action> createAdditionalActions() {
 		List<Action> additionalActions = new ArrayList<>();
+		if (newForm != null) {
+			additionalActions.add(new NewReferenceEditor());
+		}
 		boolean required = getProperty().getAnnotation(NotEmpty.class) != null;
 		if (!required && !EmptyObjects.isEmpty(getValue())) {
 			additionalActions.add(new ClearAction());
@@ -41,11 +50,38 @@ public class ReferenceFormElement<T> extends AbstractLookupFormElement {
 		return additionalActions;
 	}
 
-	private class ClearAction extends Action {
+	public ReferenceFormElement<T> newForm(Form<T> form) {
+		this.newForm = form;
+		return this;
+	}
 
+	private class ClearAction extends Action {
 		@Override
 		public void action() {
 			setValue(null);
+		}
+	}
+
+	protected class NewReferenceEditor extends NewObjectEditor<T> {
+
+		@Override
+		protected Class<T> getEditedClass() {
+			return fieldClazz;
+		}
+
+		@Override
+		protected Form<T> createForm() {
+			return newForm;
+		}
+
+		@Override
+		protected T save(T object) {
+			return Backend.save(object);
+		}
+
+		@Override
+		protected void finished(T result) {
+			ReferenceFormElement.this.setValue(result);
 			dialog.closeDialog();
 		}
 	}
