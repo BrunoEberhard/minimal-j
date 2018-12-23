bc40bdd3c7daae679e42bd84139465163c182a97
Bruno Eberhard <bruno.eberhard@pop.ch>
2018-12-23 09:41:36 +0100

    SwingReadOnlyTextField: support multi line

--- src/main/java/org/minimalj/frontend/impl/swing/toolkit/SwingReadOnlyTextField.java
+++ src/main/java/org/minimalj/frontend/impl/swing/toolkit/SwingReadOnlyTextField.java
@@ -1,9 +1,11 @@
 package org.minimalj.frontend.impl.swing.toolkit;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.UIManager;
 
 import org.minimalj.frontend.Frontend.Input;
+import org.minimalj.util.StringUtils;
 
 public class SwingReadOnlyTextField extends JLabel implements Input<String> {
 	private static final long serialVersionUID = 1L;
@@ -15,12 +17,23 @@ public class SwingReadOnlyTextField extends JLabel implements Input<String> {
 	public void updateUI() {
 		super.updateUI();
 		setBackground(UIManager.getColor("TextField.background"));
+		setBorder(BorderFactory.createLineBorder(UIManager.getColor("TextField.shadow"), 1));
 		setOpaque(true);
 	}
 
 	@Override
-	public void setValue(String text) {
-		super.setText(text);
+	public void setValue(String string) {
+		if (string != null) {
+			if (string.contains("\n")) {
+				string = StringUtils.escapeHTML(string);
+				string = string.replaceAll("\n", "<br>");
+				setText("<html><body>" + string + "</body></html>");
+			} else {
+				setText(string);
+			}
+		} else {
+			setText(null);
+		}
 	}
 
 	@Override
