1d7ca3b3e343f3eef826357adf7192e824a0f833
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-03-23 08:18:00 +0100

    ImageFormElement: better constructor signature

--- src/main/java/org/minimalj/frontend/form/element/ImageFormElement.java
+++ src/main/java/org/minimalj/frontend/form/element/ImageFormElement.java
@@ -4,18 +4,29 @@ import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.Frontend.IComponent;
 import org.minimalj.frontend.Frontend.Input;
 import org.minimalj.frontend.Frontend.InputComponentListener;
+import org.minimalj.model.properties.PropertyInterface;
 
 public class ImageFormElement extends AbstractFormElement<byte[]> {
 
 	private final Input<byte[]> input;
 	private final FormElementConstraint constraint;
 
-	public ImageFormElement(Object key, boolean editable) {
+	public ImageFormElement(byte[] key, boolean editable) {
 		this(key, editable, 3);
 	}
 	
-	public ImageFormElement(Object key, boolean editable, int size) {
+	public ImageFormElement(byte[] key, boolean editable, int size) {
 		super(key);
+		this.constraint = new FormElementConstraint(size, size);
+		input = Frontend.getInstance().createImage(editable ? new ImageFieldChangeListener() : null);
+	}
+
+	public ImageFormElement(PropertyInterface property, boolean editable) {
+		this(property, editable, 3);
+	}
+
+	public ImageFormElement(PropertyInterface property, boolean editable, int size) {
+		super(property);
 		this.constraint = new FormElementConstraint(size, size);
 		input = Frontend.getInstance().createImage(editable ? new ImageFieldChangeListener() : null);
 	}
