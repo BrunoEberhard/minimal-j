2d4b430bf8e7ec2e745bd879073f70286617d198
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-05-10 15:15:52 +0200

    Routing doc

--- ext/vaadin_springboot/src/main/java/org/minimalj/frontend/impl/vaadin/VaadinSpringboot.java
+++ ext/vaadin_springboot/src/main/java/org/minimalj/frontend/impl/vaadin/VaadinSpringboot.java
@@ -7,11 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MjVaadinSpringbootApplication {
+public class VaadinSpringboot {
 
 	private static void start() {
 		Frontend.setInstance(new VaadinFrontend());
-		SpringApplication.run(MjVaadinSpringbootApplication.class);
+		SpringApplication.run(VaadinSpringboot.class);
 	}
 	
 	public static void start(Application application) {
