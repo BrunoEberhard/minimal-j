633a0e6100f06d2120a4747818f0f0560a8274d0
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-05-26 10:21:32 +0200

    SwingHtmlContent: make picture from classLoader work.

--- src/main/java/org/minimalj/frontend/impl/swing/component/SwingHtmlContent.java
+++ src/main/java/org/minimalj/frontend/impl/swing/component/SwingHtmlContent.java
@@ -1,11 +1,21 @@
 package org.minimalj.frontend.impl.swing.component;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.swing.JTextPane;
 import javax.swing.event.HyperlinkEvent.EventType;
 import javax.swing.event.HyperlinkListener;
+import javax.swing.text.Element;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.HTMLEditorKit.HTMLFactory;
+import javax.swing.text.html.ImageView;
 
 import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.Frontend.IContent;
@@ -19,6 +29,7 @@ public class SwingHtmlContent extends JTextPane implements IContent {
 	public static final String EVENT_TYPE_CLICK = "click";
 
 	public SwingHtmlContent(String htmlOrUrl) {
+		setEditorKit(new MjHtmlEditorKit());
 		setContentType("text/html");
 		setEditable(false);
 
@@ -65,6 +76,56 @@ public class SwingHtmlContent extends JTextPane implements IContent {
 			}
 		};
 		addHyperlinkListener(listener);
+	}
+
+	private static class MjHtmlEditorKit extends HTMLEditorKit {
+		private static final long serialVersionUID = 1L;
+
+		private static ViewFactory mjFactory = new MjHTMLFactory();
+
+		public ViewFactory getViewFactory() {
+			return mjFactory;
+		}
+	}
+
+	private static class MjHTMLFactory extends HTMLFactory {
+		@Override
+		public View create(Element elem) {
+			Object tag = elem.getAttributes().getAttribute(StyleConstants.NameAttribute);
+			if (tag instanceof HTML.Tag) {
+				HTML.Tag kind = (HTML.Tag) tag;
+				if (kind == HTML.Tag.IMG) {
+					return new MjImageView(elem);
+				}
+			}
+			return super.create(elem);
+		}
+	}
+
+	private static class MjImageView extends ImageView {
+
+		public MjImageView(Element elem) {
+			super(elem);
+		}
+
+		public URL getImageURL() {
+			String src = (String) getElement().getAttributes().getAttribute(HTML.Attribute.SRC);
+			if (src == null) {
+				return null;
+			}
+
+			URL reference = ((HTMLDocument) getDocument()).getBase();
+			try {
+				if (reference != null) {
+					return new URL(reference, src);
+				} else {
+					// this is the line
+					return getClass().getClassLoader().getResource(src);
+				}
+			} catch (MalformedURLException e) {
+				return null;
+			}
+		}
 	}
 
 }
