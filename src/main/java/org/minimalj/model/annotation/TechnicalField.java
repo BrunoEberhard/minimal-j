7d16ef35dfd2c25e1337bc7a34f7a442cdb8da69
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-12-14 16:27:19 +0100

    SqlTechnicalFieldTest: relax asserts for time precision

--- src/main/java/org/minimalj/model/annotation/TechnicalField.java
+++ src/main/java/org/minimalj/model/annotation/TechnicalField.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Note that the precision of these fields depend on the used repository or
+ * database.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.METHOD})
 public @interface TechnicalField {
