3fb2c1706a9721fe2592e89f295a3ac3f028261b
Bruno Eberhard <bruno.eberhard@pop.ch>
2019-12-26 07:08:39 +0100

    ThymeRequest
    Second version

--- ext/thymeleaf/src/main/java/org/minimalj/thymeleaf/ThymeHttpHandler.java
+++ ext/thymeleaf/src/main/java/org/minimalj/thymeleaf/ThymeHttpHandler.java
@@ -5,9 +5,7 @@ import org.minimalj.frontend.impl.web.MjHttpExchange;
 import org.minimalj.frontend.impl.web.MjHttpHandler;
 import org.minimalj.frontend.impl.web.ResourcesHttpHandler;
 import org.minimalj.frontend.impl.web.WebApplication;
-import org.minimalj.util.LocaleContext;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 /**
@@ -17,8 +15,6 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
  *
  */
 public abstract class ThymeHttpHandler implements MjHttpHandler {
-	public static final String DEFAULT_PATH = null;
-
 	private final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
 	private final TemplateEngine templateEngine = new TemplateEngine();
 	private final ResourcesHttpHandler resourcesHttpHandler = new ResourcesHttpHandler();
@@ -29,34 +25,18 @@ public abstract class ThymeHttpHandler implements MjHttpHandler {
 		templateEngine.setMessageResolver(new MjMessageResolver());
 	}
 
-	/**
-	 * 
-	 * @param request contains path, parameters and variable container for ThymeLeaf
-	 * @return name of the template to be used. Must end with ".html" and be
-	 *         available in classpath. Return <code>DEFAULT_PATH</code> or
-	 *         <code>null</code> if template name should match the path.
-	 */
-	protected abstract String handle(ThymeRequest request);
+	protected abstract void handle(ThymeRequest request);
 
 	@Override
 	public final boolean handle(MjHttpExchange exchange) {
-		ThymeRequest request = new ThymeRequest(exchange);
+		ThymeRequest request = new ThymeRequest(templateEngine, exchange);
 
-		String templateName = handle(request);
-
-		if (templateName == DEFAULT_PATH) {
-			templateName = exchange.getPath();
-		}
-
-		if (!templateName.endsWith(".html")) {
+		handle(request);
+		if (request.isResponseSent()) {
+			return true;
+		} else {
 			return resourcesHttpHandler.handle(exchange);
 		}
-
-		Context context = new Context(LocaleContext.getCurrent(), request.getContext());
-		String response = templateEngine.process(templateName, context);
-		exchange.sendResponse(200, response, "text/html");
-
-		return true;
 	}
 
 }
--- ext/thymeleaf/src/main/java/org/minimalj/thymeleaf/ThymeRequest.java
+++ ext/thymeleaf/src/main/java/org/minimalj/thymeleaf/ThymeRequest.java
@@ -10,20 +10,26 @@ import org.minimalj.frontend.impl.json.JsonFrontend;
 import org.minimalj.frontend.impl.web.MjHttpExchange;
 import org.minimalj.security.Subject;
 import org.minimalj.thymeleaf.page.ThymePage.ThymePageExchange;
+import org.minimalj.util.LocaleContext;
 import org.minimalj.util.StringUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 public class ThymeRequest {
 
+	private final TemplateEngine templateEngine;
 	private final MjHttpExchange exchange;
 	private final String path;
 	private Map<String, Object> context;
+	private boolean responseSent = false;
 
-	ThymeRequest(MjHttpExchange exchange) {
+	ThymeRequest(TemplateEngine templateEngine, MjHttpExchange exchange) {
+		this.templateEngine = templateEngine;
 		this.exchange = exchange;
-		this.path = StringUtils.isEmpty(exchange.getPath()) ? "/" : "";
+		this.path = StringUtils.isEmpty(exchange.getPath()) ? "/" : exchange.getPath();
 	}
 
-	Map<String, Object> getContext() {
+	private Map<String, Object> getContext() {
 		if (context == null) {
 			context = new HashMap<>();
 
@@ -55,4 +61,22 @@ public class ThymeRequest {
 		getContext().put(key, value);
 	}
 
+	public void sendResponse() {
+		sendResponse(getPath());
+	}
+
+	public void sendResponse(String templateName) {
+		if (responseSent) {
+			throw new IllegalStateException("Already send response. Cannont use " + templateName);
+		}
+
+		Context context = new Context(LocaleContext.getCurrent(), getContext());
+		String response = templateEngine.process(templateName, context);
+		exchange.sendResponse(200, response, "text/html");
+		responseSent = true;
+	}
+
+	public boolean isResponseSent() {
+		return responseSent;
+	}
 }
