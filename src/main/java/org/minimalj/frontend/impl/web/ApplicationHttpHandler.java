0a954604dbd64764187a5aa3ca4ae56e78f6951b
Bruno Eberhard <bruno.eberhard@pop.ch>
2020-02-17 17:29:55 +0100

    ApplicationHttpHandler regression fix
    with relaxed validationRoute the resources weren't available anymore.

--- src/main/java/org/minimalj/frontend/impl/web/ApplicationHttpHandler.java
+++ src/main/java/org/minimalj/frontend/impl/web/ApplicationHttpHandler.java
@@ -24,21 +24,23 @@ public class ApplicationHttpHandler implements MjHttpHandler {
 			handle(exchange, exchange.getPath().substring(this.path.length() - 1));
 		}
 	}
-
+	
 	private void handle(MjHttpExchange exchange, String path) {
 		if (path.equals("/ajax_request.xml")) {
 			String response = JsonSessionManager.getInstance().handle(exchange.getRequest());
 			exchange.sendResponse(200, response, "application/json");
 		} else if (path.equals("/application.png")) {
 			exchange.sendResponse(200, ResourcesHttpHandler.read(Application.getInstance().getIcon()), "image/png");
-		} else if (Page.validateRoute(path)) {
-			String htmlTemplate = JsonFrontend.getHtmlTemplate();
-			htmlTemplate = htmlTemplate.replace("$SEND", WebServer.useWebSocket ? "sendWebSocket" : "sendAjax");
-			String html = JsonFrontend.fillPlaceHolder(htmlTemplate, path.length() < 2 ? "/" : path.substring(1));
-			exchange.sendResponse(200, html, "text/html");
 		} else {
 			resourcesHttpHandler.handle(exchange, path);
+
+			if (!exchange.isResponseSent() && Page.validateRoute(path)) {
+				String htmlTemplate = JsonFrontend.getHtmlTemplate();
+				htmlTemplate = htmlTemplate.replace("$SEND", WebServer.useWebSocket ? "sendWebSocket" : "sendAjax");
+				String html = JsonFrontend.fillPlaceHolder(htmlTemplate, path.length() < 2 ? "/" : path.substring(1));
+				exchange.sendResponse(200, html, "text/html");
+			}
 		}
 	}
-
+	
 }
