4dfe39fdf45e976832f19af7923afa3794528513
Bruno Eberhard <Bruno@host.docker.internal>
2020-06-28 13:56:47 +0200

    VaadinMenu cleanup

--- ext/vaadin/src/main/java/org/minimalj/frontend/impl/vaadin/toolkit/VaadinMenu.java
+++ ext/vaadin/src/main/java/org/minimalj/frontend/impl/vaadin/toolkit/VaadinMenu.java
@@ -9,51 +9,32 @@ import org.minimalj.frontend.action.Separator;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.contextmenu.HasMenuItems;
 import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.contextmenu.SubMenu;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Hr;
 
 public class VaadinMenu {
 
-	
-//	ContextMenu contextMenu = new ContextMenu();
-//
-//	Component target = createTargetComponent();
-//	contextMenu.setTarget(target);
-//
-//	Label message = new Label("-");
-//
-//	contextMenu.addItem("First menu item",
-//	        event -> message.setText("Clicked on the first item"));
-//
-//	MenuItem parent = contextMenu.addItem("Parent item");
-//	SubMenu subMenu = parent.getSubMenu();
-//
-//	subMenu.addItem("Second menu item",
-//	        event -> message.setText("Clicked on the second item"));
-//
-//	subMenu = subMenu.addItem("Parent item").getSubMenu();
-//	subMenu.addItem("Third menu item",
-//	        event -> message.setText("Clicked on the third item"));
-	
-	
-	public static void createMenu(Component target, List<Action> actions) {
+	public static ContextMenu createMenu(Component target, List<Action> actions) {
 		if (actions != null && actions.size() > 0) {
 			ContextMenu menu = new ContextMenu();
 			addActions(menu, actions);
 			menu.setTarget(target);
+			return menu;
 		}
+		return null;
 	}
 
-	private static void addActions(ContextMenu menu, List<Action> actions) {
+	public static void addActions(ContextMenu menu, List<Action> actions) {
 		for (Action action : actions) {
 			if (action instanceof ActionGroup) {
 				ActionGroup actionGroup = (org.minimalj.frontend.action.ActionGroup) action;
-				MenuItem parent = menu.addItem(actionGroup.getName());
-				SubMenu subMenu = parent.getSubMenu();
+				MenuItem groupItem = menu.addItem(actionGroup.getName());
+				SubMenu subMenu = groupItem.getSubMenu();
 				addActions(subMenu, actionGroup.getItems());
 			} else if (action instanceof Separator) {
-				menu.add(new H2("TODO"));
+				menu.add(new Hr());
 			} else {
 				adaptAction(menu, action);
 			}
@@ -61,45 +42,24 @@ public class VaadinMenu {
 	}
 
 	// no common interface between MenuItem and ContextMenu
-	private static void addActions(SubMenu menu, List<Action> actions) {
+	public static void addActions(SubMenu menu, List<Action> actions) {
 		for (Action action : actions) {
 			if (action instanceof ActionGroup) {
 				ActionGroup actionGroup = (org.minimalj.frontend.action.ActionGroup) action;
-				MenuItem parent = menu.addItem(actionGroup.getName());
-				SubMenu subMenu = parent.getSubMenu();
+				MenuItem groupItem = menu.addItem(actionGroup.getName());
+				SubMenu subMenu = groupItem.getSubMenu();
 				addActions(subMenu, actionGroup.getItems());
 			} else if (action instanceof Separator) {
-				menu.add(new H2("TODO"));
+				menu.add(new Hr());
 			} else {
 				adaptAction(menu, action);
 			}
 		}
 	}
 	
-	private static MenuItem adaptAction(SubMenu menu, Action action) {
-		MenuItem item = menu.addItem(action.getName(), e -> {
-			action.action();
-		});
-		action.setChangeListener(new ActionChangeListener() {
-			{
-				update();
-			}
-			
-			@Override
-			public void change() {
-				update();
-			}
-
-			protected void update() {
-				item.setEnabled(action.isEnabled());
-				item.setText(action.getName());
-			}
-		});
-		return item;
-	}
-
-	private static MenuItem adaptAction(ContextMenu menu, Action action) {
+	public static MenuItem adaptAction(HasMenuItems menu, Action action) {
 		MenuItem item = menu.addItem(action.getName(), e -> action.action());
+		item.setEnabled(action.isEnabled());
 		action.setChangeListener(new ActionChangeListener() {
 			{
 				update();
