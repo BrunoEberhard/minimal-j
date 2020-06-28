dcefd6d9d70a4335f8a6b4fbeb5d5baeed573700
Bruno Eberhard <Bruno@host.docker.internal>
2020-06-28 13:46:12 +0200

    VaadinVerticalLayout support HasComponent

--- ext/vaadin/src/main/java/org/minimalj/frontend/impl/vaadin/toolkit/VaadinVerticalLayout.java
+++ ext/vaadin/src/main/java/org/minimalj/frontend/impl/vaadin/toolkit/VaadinVerticalLayout.java
@@ -1,8 +1,10 @@
 package org.minimalj.frontend.impl.vaadin.toolkit;
 
 import org.minimalj.frontend.Frontend.IComponent;
+import org.minimalj.frontend.impl.vaadin.toolkit.VaadinFrontend.HasComponent;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
@@ -10,10 +12,16 @@ public class VaadinVerticalLayout extends VerticalLayout implements IComponent {
 	private static final long serialVersionUID = 1L;
 
 	public VaadinVerticalLayout(IComponent[] components) {
+		addAll(this, components);
+	}
+
+	static void addAll(HasComponents hasComponents, IComponent[] components) {
 		for (IComponent c : components) {
-			Component component = (Component) c;
-			((HasSize) component).setWidthFull();
-			add(component);
+			Component component = c instanceof HasComponent ? ((HasComponent) c).getComponent() : (Component) c;
+			if (component instanceof HasSize) {
+				((HasSize) component).setWidthFull();
+			}
+			hasComponents.add(component);
 		}
 	}
 
