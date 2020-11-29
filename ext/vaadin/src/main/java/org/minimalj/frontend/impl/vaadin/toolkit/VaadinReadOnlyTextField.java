6629eae3f6c4ed25dc9db863e06d358703ac4e1a
Bruno Eberhard <bruno.eberhard@pop.ch>
2020-11-29 20:30:19 +0100

    VaadinReadOnlyTextField (multi line)

--- ext/vaadin/src/main/java/org/minimalj/frontend/impl/vaadin/toolkit/VaadinReadOnlyTextField.java
+++ ext/vaadin/src/main/java/org/minimalj/frontend/impl/vaadin/toolkit/VaadinReadOnlyTextField.java
@@ -3,9 +3,9 @@ package org.minimalj.frontend.impl.vaadin.toolkit;
 import org.minimalj.frontend.Frontend.Input;
 import org.minimalj.frontend.impl.vaadin.toolkit.VaadinFrontend.HasCaption;
 
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextArea;
 
-public class VaadinReadOnlyTextField extends TextField implements Input<String>, HasCaption {
+public class VaadinReadOnlyTextField extends TextArea implements Input<String>, HasCaption {
 	private static final long serialVersionUID = 1L;
 	private String value;
 	
