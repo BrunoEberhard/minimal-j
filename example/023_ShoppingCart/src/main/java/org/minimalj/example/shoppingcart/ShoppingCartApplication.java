8af8330190c7d682e3aaee088a133b6371ea264c
Bruno Eberhard <bruno.eberhard@pop.ch>
2021-02-01 14:43:33 +0100

    ShoppingCartApplication: use new rest handler

--- example/023_ShoppingCart/src/main/java/org/minimalj/example/shoppingcart/ShoppingCartApplication.java
+++ example/023_ShoppingCart/src/main/java/org/minimalj/example/shoppingcart/ShoppingCartApplication.java
@@ -1,20 +1,24 @@
 package org.minimalj.example.shoppingcart;
 
-import org.minimalj.application.Application;
 import org.minimalj.application.Configuration;
+import org.minimalj.frontend.impl.web.MjHttpHandler;
+import org.minimalj.frontend.impl.web.WebApplication;
 import org.minimalj.repository.memory.InMemoryRepository;
-import org.minimalj.rest.RestServer;
+import org.minimalj.rest.RestHttpHandler;
 
-public class ShoppingCartApplication extends Application {
+public class ShoppingCartApplication extends WebApplication {
 
 	@Override
 	public Class<?>[] getEntityClasses() {
 		return new Class[] { Product.class };
 	}
 
+	@Override
+	public MjHttpHandler createHttpHandler() {
+		return new RestHttpHandler();
+	}
+	
 	public static void main(String[] args) {
 		Configuration.set("MjRepository", InMemoryRepository.class.getName());
-
-		RestServer.start(new ShoppingCartApplication());
 	}
 }
