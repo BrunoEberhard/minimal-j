578aa192adee1ab6b54cbcc0e4afc540e8699acb
Bruno Eberhard <bruno.eberhard@pop.ch>
2021-04-24 09:21:03 +0200

    Security: add frame-ancestors header

--- src/main/java/org/minimalj/frontend/impl/web/ApplicationHttpHandler.java
+++ src/main/java/org/minimalj/frontend/impl/web/ApplicationHttpHandler.java
@@ -62,6 +62,8 @@ public class ApplicationHttpHandler implements MjHttpHandler {
 	public static void handleTemplate(MjHttpExchange exchange, String path) {
 		String htmlTemplate = JsonFrontend.getHtmlTemplate();
 		String html = JsonFrontend.fillPlaceHolder(htmlTemplate, path);
+		exchange.addHeader("frame-ancestors", "frame-ancestors 'none'");
+		exchange.addHeader("frame-X-Frame-Options", "DENY");
 		exchange.sendResponse(200, html, "text/html;charset=UTF-8");
 	}
 
