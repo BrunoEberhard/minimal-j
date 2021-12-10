bbf76762dd7e9fb49857c39f8055f74265faeb1b
Bruno Eberhard <bruno.eberhard@pop.ch>
2021-12-10 14:37:31 +0100

    Remove concept of QueryResultList and SortableList
    Complicated and nearly in no case what's wanted

---
+++ src/main/java/org/minimalj/frontend/util/LazyLoadingList.java
@@ -0,0 +1,24 @@
+package org.minimalj.frontend.util;
+
+import java.util.AbstractList;
+import java.util.List;
+
+import org.minimalj.frontend.impl.util.ColumnFilter;
+
+public abstract class LazyLoadingList<T> extends AbstractList<T> {
+
+	@Override
+	public int size() {
+		throw new RuntimeException("Should not be called");
+	}
+	
+	@Override
+	public T get(int index) {
+		throw new RuntimeException("Should not be called");
+	}
+	
+	public abstract List<T> get(ColumnFilter[] filters, Object[] sortKeys, boolean[] sortDirections, int page, int pageSize);
+
+	public abstract int count(ColumnFilter[] filters);
+
+}
