d58f1986eedadef4fafb0ad470345f4064024fab
Bruno Eberhard <bruno.eberhard@pop.ch>
2022-05-24 11:23:13 +0200

    getClassesRecursive with some options

--- src/main/java/org/minimalj/model/Model.java
+++ src/main/java/org/minimalj/model/Model.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.minimalj.model.properties.Properties;
 import org.minimalj.model.properties.PropertyInterface;
+import org.minimalj.util.FieldUtils;
 import org.minimalj.util.IdUtils;
 
 public interface Model {
@@ -25,15 +26,19 @@ public interface Model {
 	 * @param baseClasses the classes from the model
 	 * @return the main entities 
 	 */
-	public static List<Class<?>> getEntityClassesRecursive(Class<?>... baseClasses) {
+	public static List<Class<?>> getEntityClassesRecursive(Class<?>[] baseClasses) {
+		return getClassesRecursive(baseClasses, false, true);
+	}
+	
+	public static List<Class<?>> getClassesRecursive(Class<?>[] baseClasses, boolean depthFirst, boolean onlyWithId) {
 		List<Class<?>> classes = new ArrayList<>();
 		for (Class<?> clazz : baseClasses) {
-			getEntityClassesRecursive(classes, clazz);
+			getClassesRecursive(classes, clazz, depthFirst, onlyWithId);
 		}
 		return classes;
 	}
 
-	private static void getEntityClassesRecursive(List<Class<?>> classes, Class<?> clazz) {
+	private static void getClassesRecursive(List<Class<?>> classes, Class<?> clazz, boolean depthFirst, boolean onlyWithId) {
 		if (!classes.contains(clazz)) {
 			classes.add(clazz);
 			for (PropertyInterface property : Properties.getProperties(clazz).values()) {
@@ -41,9 +46,13 @@ public interface Model {
 				if (Collection.class.isAssignableFrom(propertyClass)) {
 					propertyClass = property.getGenericClass();
 				}
-				if (IdUtils.hasId(propertyClass)) {
-					getEntityClassesRecursive(classes, propertyClass);
+				if (onlyWithId && IdUtils.hasId(propertyClass) || !onlyWithId && !FieldUtils.isAllowedPrimitive(propertyClass) && !property.isFinal()) {
+					getClassesRecursive(classes, propertyClass, depthFirst, onlyWithId);
 				}
+			}
+			if (depthFirst) {
+				classes.remove(clazz);
+				classes.add(clazz);
 			}
 		}
 	}
