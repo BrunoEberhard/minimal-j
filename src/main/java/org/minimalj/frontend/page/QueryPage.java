b2778c0ab7e72b3ebd56b37e12f0f5580b584711
Bruno Eberhard <bruno.eberhard@pop.ch>
2022-10-24 13:29:57 +0200

    QueryPage with actions

--- src/main/java/org/minimalj/frontend/page/QueryPage.java
+++ src/main/java/org/minimalj/frontend/page/QueryPage.java
@@ -1,14 +1,43 @@
 package org.minimalj.frontend.page;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.Frontend.IContent;
+import org.minimalj.frontend.action.Action;
+import org.minimalj.frontend.impl.json.JsonAction;
+import org.minimalj.frontend.impl.json.JsonQueryContent;
 import org.minimalj.util.resources.Resources;
 
 public class QueryPage implements Page {
 
+	private final List<Action> quickActions;
+	
+	public QueryPage() {
+		this(Collections.emptyList());
+	}
+
+	public QueryPage(List<Action> quickActions) {
+		this.quickActions = quickActions;
+	}
+	
 	@Override
 	public IContent getContent() {
-		return Frontend.getInstance().createQueryContent();
+		IContent content = Frontend.getInstance().createQueryContent();
+		if (content instanceof JsonQueryContent && !quickActions.isEmpty()) {
+			JsonQueryContent jsonQueryContent = (JsonQueryContent) content;
+
+			List<JsonAction> actionLabels = new ArrayList<>();
+			for (Action action : quickActions) {
+				actionLabels.add(new JsonAction(action));
+			}
+			if (!actionLabels.isEmpty()) {
+				jsonQueryContent.put("actions", actionLabels);
+			}
+		}
+		return content;
 	}
 	
 	@Override
