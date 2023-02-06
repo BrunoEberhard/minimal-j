190b1fe555d0cf0ce45dae458f1adb0f1b19954d
Bruno Eberhard <bruno.eberhard@pop.ch>
2023-02-06 09:32:43 +0100

    UserRole: hashCode and equals

--- src/main/java/org/minimalj/security/model/UserRole.java
+++ src/main/java/org/minimalj/security/model/UserRole.java
@@ -1,17 +1,36 @@
 package org.minimalj.security.model;
 
+import java.util.Objects;
+
 import org.minimalj.model.annotation.Size;
 
 public class UserRole {
-	
+
 	public UserRole() {
 		//
 	}
-	
+
 	public UserRole(String roleName) {
 		this.name = roleName;
 	}
-	
+
 	@Size(255)
 	public String name;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null || getClass() != object.getClass()) {
+			return false;
+		}
+		return Objects.equals(name, ((UserRole) object).name);
+	}
+
 }
