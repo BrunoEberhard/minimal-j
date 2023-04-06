aa85dd492165b1d4979d0f81ad091d51d024dd5b
Bruno Eberhard <bruno.eberhard@pop.ch>
2023-04-06 08:55:35 +0200

    CheckBoxFormElement constructor to edit one element of Set<Enum>

--- src/main/java/org/minimalj/frontend/form/element/CheckBoxFormElement.java
+++ src/main/java/org/minimalj/frontend/form/element/CheckBoxFormElement.java
@@ -1,11 +1,15 @@
 package org.minimalj.frontend.form.element;
 
+import java.lang.annotation.Annotation;
 import java.util.Objects;
 import java.util.Set;
 
 import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.Frontend.IComponent;
 import org.minimalj.frontend.Frontend.Input;
+import org.minimalj.frontend.form.Form;
+import org.minimalj.model.EnumUtils;
+import org.minimalj.model.Keys;
 import org.minimalj.model.properties.Property;
 import org.minimalj.model.properties.VirtualProperty;
 import org.minimalj.util.resources.Resources;
@@ -37,6 +41,14 @@ public class CheckBoxFormElement extends AbstractFormElement<Boolean> implements
 		this.caption = caption;
 		checkBox = Frontend.getInstance().createCheckBox(listener(), text);
 		checkBox.setEditable(editable);
+	}
+
+	public <E extends Enum<E>> CheckBoxFormElement(Set<E> key, E value, boolean editable) {
+		this(new SetElementFormElementProperty(Keys.getProperty(key), value), EnumUtils.getText(value), editable);
+	}
+
+	public <E extends Enum<E>> CheckBoxFormElement(Set<E> key, E value) {
+		this(key, value, Form.EDITABLE);
 	}
 
 	@Override
@@ -90,31 +102,59 @@ public class CheckBoxFormElement extends AbstractFormElement<Boolean> implements
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static class SetElementFormElementProperty extends CheckBoxProperty {
+		private final Property property;
 		private final Object value;
 
-		public SetElementFormElementProperty(Object value) {
+		public SetElementFormElementProperty(Property property, Object value) {
+			this.property = property;
 			this.value = Objects.requireNonNull(value);
 		}
 
 		@Override
-		public String getName() {
-			return value.toString();
-		}
-
-		@Override
 		public Boolean getValue(Object object) {
-			Set set = (Set) object;
+			Set set = (Set) property.getValue(object);
 			return set.contains(value);
 		}
 
 		@Override
 		public void setValue(Object object, Object newValue) {
-			Set set = (Set) object;
+			Set set = (Set) property.getValue(object);
 			if (Boolean.TRUE.equals(newValue)) {
 				set.add(value);
 			} else {
 				set.remove(value);
 			}
+		}
+
+		@Override
+		public Class<?> getDeclaringClass() {
+			return property.getDeclaringClass();
+		}
+
+		@Override
+		public String getName() {
+			return property.getName();
+		}
+
+		@Override
+		public String getPath() {
+			return property.getPath();
+		}
+
+		@Override
+		public Class<?> getGenericClass() {
+			return null;
+		}
+
+		@Override
+		public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+			return property.getAnnotation(annotationClass);
+		}
+
+		@Override
+		public boolean isFinal() {
+			// override, otherwise all fields would be read only
+			return false;
 		}
 	}
 
