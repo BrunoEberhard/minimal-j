e9e161ead8eb4567b519d1ad5e02e849b369ca4e
Bruno Eberhard <bruno.eberhard@pop.ch>
2024-04-30 17:10:45 +0200

    System.out removed

--- ext/metamodel/src/main/java/org/minimalj/metamodel/model/MjEntity.java
+++ ext/metamodel/src/main/java/org/minimalj/metamodel/model/MjEntity.java
@@ -99,7 +99,6 @@ public class MjEntity {
 		validatable = Validation.class.isAssignableFrom(clazz);
 		
 		if (clazz.getSuperclass() != Object.class && clazz.getSuperclass() != Enum.class) {
-			System.out.println("super entity: " + clazz.getSimpleName());
 			superEntity = model.getOrCreateEntity(clazz.getSuperclass());
 		}
 		
