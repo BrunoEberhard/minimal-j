1944e589e6094e26f34ed10eb6e6b0e1089dc967
Bruno Eberhard <bruno.eberhard@pop.ch>
2024-12-03 17:33:10 +0100

    make DashPage changeable

--- src/main/java/org/minimalj/frontend/page/DashboardPage.java
+++ src/main/java/org/minimalj/frontend/page/DashboardPage.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.minimalj.frontend.Frontend;
 import org.minimalj.frontend.Frontend.IContent;
+import org.minimalj.frontend.Frontend.SwitchContent;
 import org.minimalj.frontend.impl.json.JsonDashboardContent;
 import org.minimalj.frontend.impl.json.JsonFrontend;
 import org.minimalj.frontend.impl.swing.SwingDashboardContent;
@@ -12,17 +13,30 @@ import org.minimalj.frontend.impl.swing.toolkit.SwingFrontend;
 
 public class DashboardPage implements Page {
 
+	private SwitchContent content;
 	private final List<Page> dashes = new ArrayList<>();
 
 	public DashboardPage() {
 	}
 
-	public void addDash(Page page) {
-		dashes.add(page);
+	public void setDashes(List<Page> dashes) {
+		this.dashes.clear();
+		this.dashes.addAll(dashes);
+		if (content != null) {
+			content.show(createContent());
+		}
 	}
-
+	
 	@Override
 	public IContent getContent() {
+		if (content == null) {
+			content = Frontend.getInstance().createSwitchContent();
+			content.show(createContent());
+		}
+		return content;
+	}
+	
+	private IContent createContent() {
 		if (Frontend.getInstance() instanceof JsonFrontend) {
 			return new JsonDashboardContent(dashes);
 		} else if (Frontend.getInstance() instanceof SwingFrontend) {
