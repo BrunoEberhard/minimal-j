4e19a39d73a69abf44df46499accb70ab067614a
Bruno Eberhard <bruno.eberhard@pop.ch>
2025-03-13 18:19:37 +0100

    UploadsFormElement

--- src/main/java/org/minimalj/frontend/form/element/UploadFormElement.java
+++ src/main/java/org/minimalj/frontend/form/element/UploadFormElement.java
@@ -6,28 +6,29 @@ import org.minimalj.frontend.Frontend.Input;
 import org.minimalj.frontend.Frontend.NamedFile;
 import org.minimalj.model.properties.Property;
 
-public class UploadFormElement extends AbstractFormElement<NamedFile[]> {
+public class UploadFormElement extends AbstractFormElement<NamedFile> {
 
 	private final Input<NamedFile[]> input;
 
-	public UploadFormElement(NamedFile[] key, boolean editable) {
+	public UploadFormElement(NamedFile key, boolean editable) {
 		super(key);
-		input = Frontend.getInstance().createUpload(editable ? listener() : null, true);
+		input = Frontend.getInstance().createUpload(editable ? listener() : null, false);
 	}
 
 	public UploadFormElement(Property property, boolean editable) {
 		super(property);
-		input = Frontend.getInstance().createUpload(editable ? listener() : null, true);
+		input = Frontend.getInstance().createUpload(editable ? listener() : null, false);
 	}
 
 	@Override
-	public void setValue(NamedFile[] object) {
-		input.setValue(object);
+	public void setValue(NamedFile object) {
+		input.setValue(object != null ? new NamedFile[] { object } : new NamedFile[0]);
 	}
 
 	@Override
-	public NamedFile[] getValue() {
-		return input.getValue();
+	public NamedFile getValue() {
+		NamedFile[] namedFiles = input.getValue();
+		return namedFiles.length == 0 ? null : namedFiles[0];
 	}
 
 	@Override
