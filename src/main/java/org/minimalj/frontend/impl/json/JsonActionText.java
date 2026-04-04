4d3ccd6ed44ef3b70817457b90d740fe8ed96ec9
Bruno Eberhard <bruno.eberhard@pop.ch>
2026-04-04 10:07:15 +0200

    JsonActionText

--- src/main/java/org/minimalj/frontend/impl/json/JsonActionText.java
+++ src/main/java/org/minimalj/frontend/impl/json/JsonActionText.java
@@ -3,18 +3,16 @@ package org.minimalj.frontend.impl.json;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.minimalj.frontend.Frontend.Input;
 import org.minimalj.frontend.action.Action;
 import org.minimalj.frontend.impl.util.ActionString;
 
-public class JsonActionText extends JsonComponent implements Input<String> {
+public class JsonActionText extends JsonComponent {
 
 	public JsonActionText(ActionString actionString) {
 		super("inline");
 
 		List<Object> elements = actionString.getComponents();
 		List<JsonComponent> components = new ArrayList<>();
-		put("type", "inline");
 		for (Object e : elements) {
 			if (e instanceof String) {
 				components.add(new JsonText((String) e));
@@ -23,20 +21,5 @@ public class JsonActionText extends JsonComponent implements Input<String> {
 			}
 			put("components", components);
 		}
-	}
-
-	@Override
-	public void setValue(String string) {
-		put(JsonInputComponent.VALUE, string);
-	}
-
-	@Override
-	public String getValue() {
-		return (String) get(JsonInputComponent.VALUE);
-	}
-
-	@Override
-	public void setEditable(boolean editable) {
-		// ignored
 	}
 }
